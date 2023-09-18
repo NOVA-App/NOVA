92cc9026bd1c9912fa2ff148fde82e5e7eb470b0
limzzum <chfhddl1234@naver.com>
2023-09-19 01:44:50 +0900

    :heavy_plus_sign: Feat(user/entity) : 엔티티 매핑 클래스 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/user/domain/User.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/user/domain/User.java
@@ -1,5 +1,34 @@
 package com.sehbeomschool.nova.domain.user.domain;
 
-public class User {
+import com.sehbeomschool.nova.global.entity.BaseEntity;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long socialId;
+    private String name;
+    private String profileImg;
+
+    @Builder
+    public User(Long id, Long socialId, String name, String profileImg) {
+        this.id = id;
+        this.socialId = socialId;
+        this.name = name;
+        this.profileImg = profileImg;
+    }
 
 }
