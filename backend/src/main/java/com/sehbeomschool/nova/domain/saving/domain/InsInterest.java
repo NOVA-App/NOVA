8af51d97eba458e113342d00548773187edea079
limzzum <chfhddl1234@naver.com>
2023-09-19 01:46:14 +0900

    :heavy_plus_sign: Feat(insinterest/entity) : 엔티티 매핑 클래스 추가

---
+++ backend/src/main/java/com/sehbeomschool/nova/domain/saving/domain/InsInterest.java
@@ -0,0 +1,26 @@
+package com.sehbeomschool.nova.domain.saving.domain;
+
+import com.sehbeomschool.nova.global.entity.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class InsInterest extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "INS_INTEREST_ID")
+    private Long id;
+
+    private int period;
+    private int interest;
+
+}
