75868c13b0367ce6fead72d27ea573a4a65b77ea
limzzum <chfhddl1234@naver.com>
2023-09-19 01:49:50 +0900

    :heavy_plus_sign: Feat(saving/entity) : 엔티티 매핑 클래스 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/saving/domain/InstallmentSavings.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/saving/domain/InstallmentSavings.java
@@ -1,5 +1,57 @@
 package com.sehbeomschool.nova.domain.saving.domain;
 
-public class InstallmentSavings {
+import com.sehbeomschool.nova.domain.game.domain.Game;
+import com.sehbeomschool.nova.global.entity.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class InstallmentSavings extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "INSTALLMENT_SAVINGS_ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "GAME_ID")
+    private Game game;
+
+    @OneToOne
+    @JoinColumn(name = "INS_INTEREST_ID")
+    private InsInterest interest;
+
+    private String name;
+    private Long amount;
+    private Long totalAmount;
+    private int startAge;
+    private int endAge;
+
+    @Builder
+    public InstallmentSavings(Long id, Game game, InsInterest interest, String name, Long amount,
+        Long totalAmount, int startAge, int endAge) {
+        this.id = id;
+        this.game = game;
+        this.interest = interest;
+        this.name = name;
+        this.amount = amount;
+        this.totalAmount = totalAmount;
+        this.startAge = startAge;
+        this.endAge = endAge;
+    }
+
 
 }
