f00d8f76ceae8b4942caacda168ca5c7891dd860
Sehbeom <j3beom@gmail.com>
2023-09-18 17:08:48 +0900

    :heavy_plus_sign: Feat(game/entity): Ages Entity 생성 및 테이블 매핑
    - Ages Entity 생성 및 테이블 매핑

---
+++ backend/src/main/java/com/sehbeomschool/nova/domain/game/domain/Ages.java
@@ -0,0 +1,42 @@
+package com.sehbeomschool.nova.domain.game.domain;
+
+import com.sehbeomschool.nova.global.entity.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Ages extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "AGE_ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "GAME_ID")
+    private Game game;
+
+    private Long totalAsset;
+
+    private Integer age;
+
+    @Builder
+    public Ages(Long id, Game game, Long totalAsset, Integer age) {
+        this.id = id;
+        this.game = game;
+        this.totalAsset = totalAsset;
+        this.age = age;
+    }
+}
