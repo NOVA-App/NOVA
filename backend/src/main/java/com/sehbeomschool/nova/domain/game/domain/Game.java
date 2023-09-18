52b175a7c3fb5a3e493025d70029a65b57367bce
Sehbeom <j3beom@gmail.com>
2023-09-18 17:08:35 +0900

    :heavy_plus_sign: Feat(game/entity): Game Entity 생성 및 테이블 매핑
    - Game Entity 생성 및 테이블 매핑

--- backend/src/main/java/com/sehbeomschool/nova/domain/game/domain/Game.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/game/domain/Game.java
@@ -1,5 +1,48 @@
 package com.sehbeomschool.nova.domain.game.domain;
 
-public class Game {
+import com.sehbeomschool.nova.global.entity.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Game extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "GAME_ID")
+    private Long id;
+
+    // TODO: User Entity ManyToOne 연결
+
+    // TODO: Gender Enum 추가
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ANALYSIS_COMMENT_ID")
+    private AnalysisComment analysisComment;
+
+    private Integer startSalary;
+    private Long resultAssets;
+    private Integer currentAge;
+
+    @Builder
+    public Game(Long id, AnalysisComment analysisComment, Integer startSalary, Long resultAssets,
+        Integer currentAge) {
+        this.id = id;
+        this.analysisComment = analysisComment;
+        this.startSalary = startSalary;
+        this.resultAssets = resultAssets;
+        this.currentAge = currentAge;
+    }
 }
