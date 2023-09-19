1dd9e34111b8efa58096b5caa7c5de6d67b97b34
임대영 <ldy092000@gmail.com>
2023-09-19 10:10:15 +0900

    :heavy_plus_sign: Feat(News/entity): News 엔티티 매핑 - NewsInfo

---
+++ backend/src/main/java/com/sehbeomschool/nova/domain/news/domain/NewsInfo.java
@@ -0,0 +1,31 @@
+package com.sehbeomschool.nova.domain.news.domain;
+
+import com.sehbeomschool.nova.domain.game.domain.Game;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NewsInfo {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "NEWS_INFO_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "NEWS_ID")
+    private News news;
+
+    @ManyToOne
+    @JoinColumn(name = "GAME_ID")
+    private Game game;
+}
