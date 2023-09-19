792ce064b37e8cae2ed898e1290b42fc0dcfd840
Sehbeom <j3beom@gmail.com>
2023-09-19 16:40:12 +0900

    :heavy_plus_sign: Feat(game): 연봉 인상률 상수 값 추가
    - SALARY_INCREASE_RATE 추가

--- backend/src/main/java/com/sehbeomschool/nova/global/constant/FixedValues.java
+++ backend/src/main/java/com/sehbeomschool/nova/global/constant/FixedValues.java
@@ -3,7 +3,8 @@ package com.sehbeomschool.nova.global.constant;
 public enum FixedValues {
     LIVING_COST_MIN(6_000_000D),
     MONTHLY_RENT_COST(6_000_000D),
-    START_AGE(27D);
+    START_AGE(27D),
+    SALARY_INCREASE_RATE(5D);
 
 
     private final Double value;
