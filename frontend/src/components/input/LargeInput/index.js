376669f903df8812b062780db48446747c74b5e9
suchshin <scshin97@gmail.com>
2023-09-19 10:59:26 +0900

    :heavy_plus_sign: Feat(common/input): 공통 인풋 생성

--- frontend/src/components/input/LargeInput/index.js
+++ frontend/src/components/input/LargeInput/index.js
@@ -2,18 +2,29 @@ import React from "react";
 import PropTypes from 'prop-types';
 import * as S from './style';
 
-const InputLarge = ({ placeholder, value, onChangeText, onSubmitEditing }) => {
+const InputLarge = (props) => {
+  const handleTextChange = (text) => {
+    // 숫자로 변환 시도
+    const numericValue = parseInt(text, 10);
+
+    // 정수로 변환된 값이 유효한 경우에만 콜백 호출
+    if (!isNaN(numericValue)) {
+      props.onChangeText(numericValue.toString());
+    }
+  };
+
   return (
     <S.StyledInputLarge
-      placeholder={placeholder}
+      placeholder={props.placeholder}
       maxLength={50}
       autoCapitalize='none'
       autoCorrect={false}
       returnKeyType='done'
       keyboardAppearance='dark'
-      value={value}
-      onChangeText={onChangeText}
-      onSubmitEditing={onSubmitEditing}
+      value={props.value}
+      onChangeText={handleTextChange} // 수정된 핸들러로 변경
+      onSubmitEditing={props.onSubmitEditing}
+      keyboardType="numeric" // 숫자 키패드 사용
     />
   );
 };
--- frontend/src/components/input/SmallInput/index.js
+++ frontend/src/components/input/SmallInput/index.js
@@ -2,18 +2,26 @@ import React from "react";
 import PropTypes from 'prop-types';
 import * as S from './style'; 
 
-const InputSmall = ({ placeholder, value, onChangeText, onSubmitEditing }) => {
+const InputSmall = (props) => {
   return (
     <S.StyledInputSmall
-      placeholder={placeholder}
+      placeholder={props.placeholder}
       maxLength={50}
       autoCapitalize='none'
       autoCorrect={false}
       returnKeyType='done'
       keyboardAppearance='dark'
-      value={value}
-      onChangeText={onChangeText}
-      onSubmitEditing={onSubmitEditing}
+      value={props.value}
+      onChangeText={(text) => {
+        // 숫자로 변환 시도
+        const numericValue = parseInt(text, 10);
+
+        // 정수로 변환된 값이 유효한 경우에만 콜백 호출
+        if (!isNaN(numericValue)) {
+          props.onChangeText(numericValue.toString());
+        }
+      }}
+      onSubmitEditing={props.onSubmitEditing}
     />
   );
 };
