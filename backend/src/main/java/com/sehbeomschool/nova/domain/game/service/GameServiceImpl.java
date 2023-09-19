f40696a442858f6de86eb2c32915c07336ca43b2
Sehbeom <j3beom@gmail.com>
2023-09-19 16:02:53 +0900

    :heavy_plus_sign: Feat(game): 게임 시작 service 계층 구현
    - createGame 구현

--- backend/src/main/java/com/sehbeomschool/nova/domain/game/service/GameServiceImpl.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/game/service/GameServiceImpl.java
@@ -1,12 +1,19 @@
 package com.sehbeomschool.nova.domain.game.service;
 
+import com.sehbeomschool.nova.domain.game.domain.Ages;
+import com.sehbeomschool.nova.global.constant.FixedValues;
 import com.sehbeomschool.nova.domain.game.dao.AgesRepository;
 import com.sehbeomschool.nova.domain.game.dao.AnalysisCommentRepository;
+import com.sehbeomschool.nova.domain.game.dao.AnnualCostRepository;
 import com.sehbeomschool.nova.domain.game.dao.EventRepository;
 import com.sehbeomschool.nova.domain.game.dao.GameRepository;
-import com.sehbeomschool.nova.domain.game.dao.MonthlyCostRepository;
 import com.sehbeomschool.nova.domain.game.dao.MyAssetsRepository;
 import com.sehbeomschool.nova.domain.game.dao.OldAgeMonthlyAssetsRepository;
+import com.sehbeomschool.nova.domain.game.domain.AnnualCost;
+import com.sehbeomschool.nova.domain.game.domain.Game;
+import com.sehbeomschool.nova.domain.game.domain.MyAssets;
+import com.sehbeomschool.nova.domain.game.dto.GameRequestDto.GameStartRequestDto;
+import com.sehbeomschool.nova.domain.game.dto.GameResponseDto.GameStartResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +25,27 @@ public class GameServiceImpl implements GameService {
     private final AnalysisCommentRepository analysisCommentRepository;
     private final EventRepository eventRepository;
     private final GameRepository gameRepository;
-    private final MonthlyCostRepository monthlyCostRepository;
+    private final AnnualCostRepository annualCostRepository;
     private final MyAssetsRepository myAssetsRepository;
     private final OldAgeMonthlyAssetsRepository oldAgeMonthlyAssetsRepository;
 
+    @Override
+    public GameStartResponseDto createGame(GameStartRequestDto gameStartRequestDto) {
+        AnnualCost annualCost = AnnualCost.createStartAnnualCost();
+
+        // TODO: 2023-09-19 User 객체 추가 
+        Game game = Game.builder()
+            .myAssets(MyAssets.createStartMyAsset(gameStartRequestDto.getStartSalary(), annualCost))
+            .annualCost(annualCost)
+            .startSalary(gameStartRequestDto.getStartSalary())
+            .gender(gameStartRequestDto.getGender())
+            .currentAge(FixedValues.START_AGE.getValue())
+            .build();
+
+        game.addAgeAndSetThis(Ages.createStartAge(gameStartRequestDto.getStartSalary()));
+
+        gameRepository.save(game);
+
+        return GameStartResponseDto.builder().gameId(game.getId()).build();
+    }
 }
