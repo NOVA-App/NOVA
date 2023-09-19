00c82e53704d48c2eaa830694183ee46a48dc3f6
임대영 <ldy092000@gmail.com>
2023-09-19 14:59:45 +0900

    :heavy_plus_sign: Feat(news/entity): news-entity - News enum Type 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/news/domain/StockNews.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/news/domain/StockNews.java
@@ -22,8 +22,8 @@ public class StockNews extends News {
     private Stock stock;
 
     @Builder
-    public StockNews(Long id, String content, Stock stock) {
-        super(id, content);
+    public StockNews(Long id, String content, Prediction prediction, Stock stock) {
+        super(id, content, prediction);
         this.stock = stock;
     }
 }
