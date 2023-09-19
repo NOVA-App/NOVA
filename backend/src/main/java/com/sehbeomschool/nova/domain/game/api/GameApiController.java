ec5ea101cb8a48670ad166ad7df3be9ad61f1db2
Sehbeom <j3beom@gmail.com>
2023-09-19 10:05:23 +0900

    :heavy_plus_sign: Feat(game): api controller 생성
    - GameApiController 생성

--- backend/src/main/java/com/sehbeomschool/nova/domain/game/api/GameApiController.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/game/api/GameApiController.java
@@ -1,5 +1,12 @@
 package com.sehbeomschool.nova.domain.game.api;
 
+import com.sehbeomschool.nova.domain.game.service.GameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class GameApiController {
 
+    private final GameService gameService;
 }
