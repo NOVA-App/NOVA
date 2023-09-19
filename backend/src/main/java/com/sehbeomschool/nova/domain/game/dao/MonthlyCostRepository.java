444b18742e52c5d1bcf0e6e053bcfb3cd3434d21
Sehbeom <j3beom@gmail.com>
2023-09-19 10:08:05 +0900

    :heavy_plus_sign: Feat(game): monthly cost repository 생성 및 설정
    - MonthlyCostRepository 생성 및 설정

---
+++ backend/src/main/java/com/sehbeomschool/nova/domain/game/dao/MonthlyCostRepository.java
@@ -0,0 +1,8 @@
+package com.sehbeomschool.nova.domain.game.dao;
+
+import com.sehbeomschool.nova.domain.game.domain.MonthlyCost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MonthlyCostRepository extends JpaRepository<MonthlyCost, Long> {
+
+}
