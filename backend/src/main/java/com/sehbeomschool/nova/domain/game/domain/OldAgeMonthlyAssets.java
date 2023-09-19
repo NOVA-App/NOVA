9c8591148c894c0c47beb0d6fea1db509bb9a5ce
Sehbeom <j3beom@gmail.com>
2023-09-19 10:55:52 +0900

    :hammer: Refactor(game): 연관관계 재설정
    - Game - MonthlyCost 연관관계 재설정
    - Game - MyAssets 연관관계 재설정
    - Game - OldAgeMonthlyAssets 연관관계 재설정

--- backend/src/main/java/com/sehbeomschool/nova/domain/game/domain/OldAgeMonthlyAssets.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/game/domain/OldAgeMonthlyAssets.java
@@ -3,12 +3,9 @@ package com.sehbeomschool.nova.domain.game.domain;
 import com.sehbeomschool.nova.global.entity.BaseEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,10 +21,6 @@ public class OldAgeMonthlyAssets extends BaseEntity {
     @Column(name = "OLD_AGE_MONTHLY_ASSET_ID")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "GAME_ID")
-    private Game game;
-
     private Integer monthlyAmount;
 
     private Integer childAllowance;
@@ -39,10 +32,9 @@ public class OldAgeMonthlyAssets extends BaseEntity {
     private Integer totalMonthlyAsset;
 
     @Builder
-    public OldAgeMonthlyAssets(Long id, Game game, Integer monthlyAmount, Integer childAllowance,
+    public OldAgeMonthlyAssets(Long id, Integer monthlyAmount, Integer childAllowance,
         Integer realtyRentIncome, Integer nationalPension, Integer totalMonthlyAsset) {
         this.id = id;
-        this.game = game;
         this.monthlyAmount = monthlyAmount;
         this.childAllowance = childAllowance;
         this.realtyRentIncome = realtyRentIncome;
