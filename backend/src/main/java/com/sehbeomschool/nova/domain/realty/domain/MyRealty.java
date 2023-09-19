b2a2ac88117ea247fe7b87882387b9b78bbc9784
임대영 <ldy092000@gmail.com>
2023-09-19 10:52:26 +0900

    :heavy_plus_sign: Feat(realty/entity): realty 엔티티 매핑

---
+++ backend/src/main/java/com/sehbeomschool/nova/domain/realty/domain/MyRealty.java
@@ -0,0 +1,47 @@
+package com.sehbeomschool.nova.domain.realty.domain;
+
+import com.sehbeomschool.nova.domain.game.domain.Game;
+import com.sehbeomschool.nova.global.entity.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MyRealty extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MY_REALTY_ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "GAME_ID")
+    private Game game;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "REALTY_ID")
+    private Realty realty;
+
+    private Long investAmount;
+
+    private Long rentIncome;
+
+    @Builder
+    public MyRealty(Long id, Game game, Realty realty, Long investAmount, Long rentIncome) {
+        this.id = id;
+        this.game = game;
+        this.realty = realty;
+        this.investAmount = investAmount;
+        this.rentIncome = rentIncome;
+    }
+}
