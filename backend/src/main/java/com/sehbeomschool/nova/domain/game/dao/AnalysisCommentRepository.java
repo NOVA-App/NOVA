6688a0c934c07569a6588d0f412a0f70ca908e4e
Sehbeom <j3beom@gmail.com>
2023-09-19 10:07:32 +0900

    :heavy_plus_sign: Feat(game): analysis comment repository 생성 및 설정
    - AnalysisCommentRepository 생성 및 설정

---
+++ backend/src/main/java/com/sehbeomschool/nova/domain/game/dao/AnalysisCommentRepository.java
@@ -0,0 +1,8 @@
+package com.sehbeomschool.nova.domain.game.dao;
+
+import com.sehbeomschool.nova.domain.game.domain.AnalysisComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnalysisCommentRepository extends JpaRepository<AnalysisComment, Long> {
+
+}
