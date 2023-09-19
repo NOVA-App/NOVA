e32db92f6c6b78455c0d6f21a4d5080bed8a7207
Sehbeom <j3beom@gmail.com>
2023-09-19 16:03:23 +0900

    :heavy_plus_sign: Feat(game): 게임 시작 controller 계층 구현
    - createGame 구현
    - 게임 시작 응답 메세지 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/game/api/GameApiController.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/game/api/GameApiController.java
@@ -1,12 +1,33 @@
 package com.sehbeomschool.nova.domain.game.api;
 
+import com.sehbeomschool.nova.domain.game.constant.GameResponseMessage;
+import com.sehbeomschool.nova.domain.game.dto.GameRequestDto.GameStartRequestDto;
+import com.sehbeomschool.nova.domain.game.dto.GameResponseDto.GameStartResponseDto;
 import com.sehbeomschool.nova.domain.game.service.GameService;
+import com.sehbeomschool.nova.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/game")
 public class GameApiController {
 
     private final GameService gameService;
+
+    @PostMapping("")
+    public ResponseEntity<ResponseDto<GameStartResponseDto>> createGame(@RequestBody
+    GameStartRequestDto gameStartRequestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+            ResponseDto.create(
+                GameResponseMessage.GAME_START_SUCCESS.getMessage(),
+                gameService.createGame(gameStartRequestDto)
+            )
+        );
+    }
 }
