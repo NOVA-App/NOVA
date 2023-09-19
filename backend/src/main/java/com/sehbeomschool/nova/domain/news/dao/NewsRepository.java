83df69613d853afb53cc0b53990611cf870c738d
임대영 <ldy092000@gmail.com>
2023-09-19 15:58:54 +0900

    :heavy_plus_sign: Feat(news/repo): news-repo 작성

--- backend/src/main/java/com/sehbeomschool/nova/domain/news/dao/NewsRepository.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/news/dao/NewsRepository.java
@@ -8,9 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface NewsRepository extends JpaRepository<News, Long> {
 
-    @Query("SELECT n FROM News n JOIN StockNews sn ON n.id = sn.id JOIN Stock s ON sn.id = s.id WHERE s.id = :stockId AND n.prediction = :prediction")
+    @Query("SELECT n FROM News n JOIN StockNews sn ON n.id = sn.id JOIN Stock s ON sn.stock.id = s.id WHERE s.id = :stockId AND n.prediction = :prediction")
     List<News> findByStockAndPrediction(@Param("stockId") Long stockId, @Param("prediction") String prediction);
 
-    @Query("SELECT n FROM News n JOIN RealtyNews rn ON n.id = rn.id JOIN Realty r ON rn.id = r.id WHERE r.id = :realtyId AND n.prediction = :prediction")
+    @Query("SELECT n FROM News n JOIN RealtyNews rn ON n.id = rn.id JOIN Realty r ON rn.realty.id = r.id WHERE r.id = :realtyId AND n.prediction = :prediction")
     List<News> findByRealtyAndPrediction(@Param("realtyId") Long realtyId, @Param("prediction") String prediction);
 }
