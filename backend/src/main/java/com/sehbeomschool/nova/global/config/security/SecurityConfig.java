baebedb050b12f175a4069bf43d704ddb8ae7008
limzzum <chfhddl1234@naver.com>
2023-09-19 09:59:23 +0900

    :heavy_plus_sign: Feat : SecurityConfig 추가

--- backend/src/main/java/com/sehbeomschool/nova/global/config/security/SecurityConfig.java
+++ backend/src/main/java/com/sehbeomschool/nova/global/config/security/SecurityConfig.java
@@ -1,5 +1,41 @@
 package com.sehbeomschool.nova.global.config.security;
 
+import com.sehbeomschool.nova.domain.user.service.UserService;
+import com.sehbeomschool.nova.global.filter.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserService userService;
+    private final String secretKey;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+            .httpBasic().disable()
+            .csrf().disable()
+            .cors().and()
+            .authorizeRequests()
+            .antMatchers("/api/user/oauth/kakao").permitAll()
+            .antMatchers(HttpMethod.POST, "/api/**").authenticated()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .addFilterBefore(new JwtFilter(userService, secretKey),
+                UsernamePasswordAuthenticationFilter.class)
+            .build();
+    }
 
 }
