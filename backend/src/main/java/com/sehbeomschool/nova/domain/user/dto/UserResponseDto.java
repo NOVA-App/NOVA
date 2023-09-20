728070b201b5cca4e4b93455e9a1718ee32ed474
임정현 <chfhddl1234@naver.com>
2023-09-20 17:46:29 +0900

    :heavy_plus_sign: Feat(user/dto) : 로그인 응답 dto 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/user/dto/UserResponseDto.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/user/dto/UserResponseDto.java
@@ -1,5 +1,15 @@
 package com.sehbeomschool.nova.domain.user.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
 public class UserResponseDto {
 
+    @Builder
+    public static class LoginResponseDto{
+        private String accessToken;
+        private String refreshToken;
+
+    }
 }
