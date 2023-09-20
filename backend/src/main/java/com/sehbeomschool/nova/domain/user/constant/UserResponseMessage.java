65e604c9477b1eb84df82a90bea019bd979c3943
임정현 <chfhddl1234@naver.com>
2023-09-20 17:50:32 +0900

    :heavy_plus_sign: Feat(user/응답메시지) : 로그인 응답 메시지 enum 생성

--- backend/src/main/java/com/sehbeomschool/nova/domain/user/constant/UserResponseMessage.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/user/constant/UserResponseMessage.java
@@ -1,4 +1,15 @@
 package com.sehbeomschool.nova.domain.user.constant;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum UserResponseMessage {
+    LOGIN_SUCCESS("로그인 성공"),
+    LOGIN_FAIL("로그인 성공"),
+    ACCESS_TOKEN_EXPIRED("토큰 만료됨");
+
+    private final String message;
+
 }
