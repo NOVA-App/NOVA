3c04f620ca67ebc187a06062402b15204957d446
임대영 <ldy092000@gmail.com>
2023-09-20 09:22:00 +0900

    :sparkles: Feat(news/api): news 조회 api URI변경

--- backend/src/main/java/com/sehbeomschool/nova/domain/news/api/NewsApiController.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/news/api/NewsApiController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/news")
+@RequestMapping("/api//news")
 public class NewsApiController {
 
     private final NewsService newsService;
