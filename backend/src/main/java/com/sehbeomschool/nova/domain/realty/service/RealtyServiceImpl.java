36cf6b5c6e61623d933aa645ac5319b0a03da896
임대영 <ldy092000@gmail.com>
2023-09-21 16:35:07 +0900

    :heavy_plus_sign: Feat(realty/api): 내 부동산 보유 현황 조회 api 작성

--- backend/src/main/java/com/sehbeomschool/nova/domain/realty/service/RealtyServiceImpl.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/realty/service/RealtyServiceImpl.java
@@ -1,5 +1,62 @@
 package com.sehbeomschool.nova.domain.realty.service;
 
-public class RealtyServiceImpl {
+import com.sehbeomschool.nova.domain.realty.dao.MyRealtyRepository;
+import com.sehbeomschool.nova.domain.realty.dao.RealtyInfoRepository;
+import com.sehbeomschool.nova.domain.realty.dao.RealtyRepository;
+import com.sehbeomschool.nova.domain.realty.domain.MyRealty;
+import com.sehbeomschool.nova.domain.realty.domain.RealtyInfo;
+import com.sehbeomschool.nova.domain.realty.dto.RealtyResponseDto.MyRealtyResponseDto;
+import com.sehbeomschool.nova.domain.realty.dto.RealtyResponseDto.ReadMyRealtyResponseDto;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
+public class RealtyServiceImpl implements RealtyService {
+
+    private final RealtyRepository realtyRepository;
+    private final RealtyInfoRepository realtyInfoRepository;
+    private final MyRealtyRepository myRealtyRepository;
+
+    @Override
+    public ReadMyRealtyResponseDto readMyRealty(Long gameId) {
+        Long investAmounts = 0L;
+        Long evaluationAmounts = 0L;
+        Long totalRentalIncome = 0L;
+
+        List<MyRealty> myRealties = myRealtyRepository.findMyRealtiesByGameId(gameId);
+
+        List<MyRealtyResponseDto> list = new ArrayList<>();
+
+        for (MyRealty mr : myRealties) {
+            RealtyInfo ri = realtyInfoRepository.findRealtyInfoByGameIdAndRealtyId(gameId,
+                mr.getRealty().getId());
+
+            investAmounts += mr.getInvestAmount();
+            evaluationAmounts += ri.getCurrentPrice();
+            totalRentalIncome += ri.getPredictedRentIncome();
+
+            MyRealtyResponseDto myRealtyResponseDto = MyRealtyResponseDto.builder()
+                .realtyId(mr.getRealty().getId())
+                .realtyName(mr.getRealty().getName())
+                .realtyImg(mr.getRealty().getRealtyImg())
+                .investAmount(mr.getInvestAmount())
+                .evaluationAmount(ri.getCurrentPrice())
+                .reantalIncome(ri.getPredictedRentIncome())
+                .build();
+
+            list.add(myRealtyResponseDto);
+        }
+
+        ReadMyRealtyResponseDto dto = ReadMyRealtyResponseDto.builder()
+            .investAmounts(investAmounts)
+            .evaluationAmounts(evaluationAmounts)
+            .totalRentalIncome(totalRentalIncome)
+            .myRealties(list)
+            .build();
+
+        return dto;
+    }
 }
