1181e89775bcd07f5c4a282914afd5b674e99e99
임대영 <ldy092000@gmail.com>
2023-09-21 14:47:39 +0900

    :heavy_plus_sign: Feat(stock/api): 내 주식 목록 조회 DTO, message 작성

--- backend/src/main/java/com/sehbeomschool/nova/domain/stock/dto/StockResponseDto.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/stock/dto/StockResponseDto.java
@@ -8,6 +8,7 @@ public class StockResponseDto {
 
     @Getter
     public static class readStocksListResponseDto {
+
         private Long stockId;
         private String stockName;
         private Long evaluationAmount;
@@ -25,6 +26,7 @@ public class StockResponseDto {
 
     @Getter
     public static class readStockDetailResponseDto {
+
         private String stockName;
         private Long evaluation;
         private Long fluctuations;
@@ -42,5 +44,48 @@ public class StockResponseDto {
         }
     }
 
+    @Getter
+    public static class readMyStocksResponseDto {
+
+        private Long investAmounts;
+        private Long evaluationAmounts;
+        private Long depreciation;
+        private Long depreciationPercent;
+        private List<MyStockResponseDto> myStocks;
+
+        @Builder
+        public readMyStocksResponseDto(Long investAmounts, Long evaluationAmounts,
+            Long depreciation,
+            Long depreciationPercent, List<MyStockResponseDto> myStocks) {
+            this.investAmounts = investAmounts;
+            this.evaluationAmounts = evaluationAmounts;
+            this.depreciation = depreciation;
+            this.depreciationPercent = depreciationPercent;
+            this.myStocks = myStocks;
+        }
+    }
+
+    @Getter
+    public static class MyStockResponseDto {
+
+        private Long stockId;
+        private String stockName;
+        private Long evaluationAmount;
+        private Long quantity;
+        private Long fluctuations;
+        private Long fluctuationsPercent;
+
+        @Builder
+        public MyStockResponseDto(Long stockId, String stockName,
+            Long evaluationAmount,
+            Long quantity, Long fluctuations, Long fluctuationsPercent) {
+            this.stockId = stockId;
+            this.stockName = stockName;
+            this.evaluationAmount = evaluationAmount;
+            this.quantity = quantity;
+            this.fluctuations = fluctuations;
+            this.fluctuationsPercent = fluctuationsPercent;
+        }
+    }
 
 }
