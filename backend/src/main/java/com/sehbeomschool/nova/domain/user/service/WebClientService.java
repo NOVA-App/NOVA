32cc83df14ff7fa91c38208b0d479834faee5a9c
임정현 <chfhddl1234@naver.com>
2023-09-21 17:44:22 +0900

    :hammer: Refactor(user/kakao) : getKakaoUserInfo 반환타입 변경

--- backend/src/main/java/com/sehbeomschool/nova/domain/user/service/WebClientService.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/user/service/WebClientService.java
@@ -1,10 +1,11 @@
 package com.sehbeomschool.nova.domain.user.service;
 
 
+import com.sehbeomschool.nova.domain.user.dto.KakaoUserInfoDto;
 import java.util.Map;
 
 public interface WebClientService {
-    public Map<String, Object> getKakaoUserInfo(String code);
+    public KakaoUserInfoDto getKakaoUserInfo(String code);
 
 
 }
