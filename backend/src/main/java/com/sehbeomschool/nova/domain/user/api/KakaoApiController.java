6fd5cc7452529a2819df91fc838c3970047f7093
임정현 <chfhddl1234@naver.com>
2023-09-21 17:32:29 +0900

    :heavy_plus_sign: Feat(user/카카오) : 카카오 로그인 api 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/user/api/KakaoApiController.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/user/api/KakaoApiController.java
@@ -1,8 +1,14 @@
 package com.sehbeomschool.nova.domain.user.api;
 
 import com.sehbeomschool.nova.domain.user.constant.UserResponseMessage;
+import com.sehbeomschool.nova.domain.user.domain.User;
+import com.sehbeomschool.nova.domain.user.dto.KakaoUserInfoDto;
 import com.sehbeomschool.nova.domain.user.dto.UserResponseDto.LoginResponseDto;
+import com.sehbeomschool.nova.domain.user.service.UserService;
+import com.sehbeomschool.nova.domain.user.service.WebClientService;
 import com.sehbeomschool.nova.global.dto.ResponseDto;
+import com.sehbeomschool.nova.global.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +18,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/oauth/kakao")
+@RequiredArgsConstructor
 public class KakaoApiController {
+
+    private final WebClientService webClientService;
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
 
     @GetMapping("/callback")
     public ResponseEntity<ResponseDto<LoginResponseDto>> kakaoLogin(@RequestParam String code) {
+        KakaoUserInfoDto kakaoUserInfo = webClientService.getKakaoUserInfo(code);
+
+        if (!userService.isExistUser(kakaoUserInfo.getId())) {
+            userService.createUser(kakaoUserInfo);
+        }
+
+        User user = userService.readUserBySocialId(kakaoUserInfo.getId());
+
+        String accessToken = jwtUtil.createJwtToken(user.getId());
+        String refreshToken = jwtUtil.createRefreshToken(user.getId());
+
         return ResponseEntity.status(HttpStatus.ACCEPTED)
             .body(ResponseDto.create(UserResponseMessage.LOGIN_SUCCESS.getMessage(),
-                LoginResponseDto.builder().accessToken("").refreshToken("").build()));
+                LoginResponseDto.builder().accessToken(accessToken).refreshToken(refreshToken)
+                    .build()));
     }
+
+
 }
