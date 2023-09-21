728114aa6a8e7f06a2686b5699ddc9612b47a2ce
임대영 <ldy092000@gmail.com>
2023-09-21 16:34:35 +0900

    :heavy_plus_sign: Feat(realty/repo): 부동산 repo 작성

---
+++ backend/src/main/java/com/sehbeomschool/nova/domain/realty/dao/MyRealtyRepository.java
@@ -0,0 +1,10 @@
+package com.sehbeomschool.nova.domain.realty.dao;
+
+import com.sehbeomschool.nova.domain.realty.domain.MyRealty;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MyRealtyRepository extends JpaRepository<MyRealty, Long> {
+
+    List<MyRealty> findMyRealtiesByGameId(Long gameId);
+}
