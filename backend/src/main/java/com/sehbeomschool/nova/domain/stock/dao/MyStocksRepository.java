d8a24d7a7247652330a02fe355ff42bc18a3c01f
임대영 <ldy092000@gmail.com>
2023-09-21 14:46:57 +0900

    :heavy_plus_sign: Feat(stock/api): 내 주식 목록 조회 repo 작성

--- backend/src/main/java/com/sehbeomschool/nova/domain/stock/dao/MyStocksRepository.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/stock/dao/MyStocksRepository.java
@@ -2,6 +2,7 @@ package com.sehbeomschool.nova.domain.stock.dao;
 
 import com.sehbeomschool.nova.domain.stock.domain.MyStocks;
 import io.lettuce.core.dynamic.annotation.Param;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,4 +10,7 @@ public interface MyStocksRepository extends JpaRepository<MyStocks, Long> {
 
     @Query("SELECT ms.quantity FROM MyStocks ms WHERE ms.game.id = :gameId AND ms.stock.id = :stockId")
     Long findQuantityByGameIdAndStockId(@Param("gameId") Long gameId, @Param("stockId") Long stockId);
+
+
+    List<MyStocks> findMyStocksByGameId(Long gameId);
 }
