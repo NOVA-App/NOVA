eb81f193e2343260d071234d1eae8e624df0ddbd
임정현 <chfhddl1234@naver.com>
2023-09-21 17:43:10 +0900

    :heavy_plus_sign: Feat(user/kakao) : webflux 유저정보 받기 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/user/service/WebClientServiceImpl.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/user/service/WebClientServiceImpl.java
@@ -1,6 +1,8 @@
 package com.sehbeomschool.nova.domain.user.service;
 
+import com.sehbeomschool.nova.domain.user.dto.KakaoUserInfoDto;
 import java.util.Map;
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -16,12 +18,15 @@ public class WebClientServiceImpl implements WebClientService {
     @Value("${kakao.url.token}")
     private String tokenUrl;
 
+    @Value("${kakao.url.profile}")
+    private String infoUrl;
+
     @Value("${kakao.redirect}")
     private String redirectUrl;
 
 
     @Override
-    public Map<String, Object> getKakaoUserInfo(String code) {
+    public KakaoUserInfoDto getKakaoUserInfo(String code) {
         WebClient webClient =
             WebClient
                 .builder()
@@ -35,12 +40,28 @@ public class WebClientServiceImpl implements WebClientService {
                     .queryParam("code", code)
                     .queryParam("grant_type", "authorization_code")
                     .queryParam("client_id", clientId)
+                    .queryParam("redirect_uri", redirectUrl)
                     .build())
             .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
             .retrieve()
             .bodyToMono(Map.class)
             .block();
 
-        return result;
+        String accessToken = String.valueOf(result.get("access_token"));
+        String refreshToken = String.valueOf(result.get("access_token"));
+
+        result = webClient
+            .get()
+            .uri(uriBuilder ->
+                uriBuilder
+                    .path(infoUrl)
+                    .build())
+            .header(HttpHeaders.AUTHORIZATION, "Bearer "+accessToken)
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+            .retrieve()
+            .bodyToMono(Map.class)
+            .block();
+
+        return new KakaoUserInfoDto(result);
     }
 }
