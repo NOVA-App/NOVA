7fc367c3a0a07cab019f2220384701de9fac4ce4
임대영 <ldy092000@gmail.com>
2023-09-21 13:00:23 +0900

    :heavy_plus_sign: Feat(stock/api): stock 목록, 디테일 조회 api 작성

--- backend/src/main/java/com/sehbeomschool/nova/domain/stock/constant/StockResponseMessage.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/stock/constant/StockResponseMessage.java
@@ -1,16 +1,14 @@
 package com.sehbeomschool.nova.domain.stock.constant;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public enum StockResponseMessage {
     READ_STOCKS_LIST("주식 종목 반환"),
     READ_STOCK_DETAIL("주식 종목 상세 반환 완료");
 
     private final String message;
-
-    StockResponseMessage(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+    
 }
