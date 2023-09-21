1d21fee9d4b7f778a33c1a39b3940a977d27956c
김의준 <euijun98@naver.com>
2023-09-21 16:53:36 +0900

    :heavy_plus_sign: Feat(main/UI): 메인페이지 UI 구현

---
+++ frontend/src/components/mainpage/MyAsset/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex-direction: row;
+  align-items: center;
+  border-radius: 20px;
+  background-color: #e7f7f6;
+`;
