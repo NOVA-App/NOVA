e283ac0be806979f7e465a2735ea2a16c6f93769
임정현 <chfhddl1234@naver.com>
2023-09-21 17:41:32 +0900

    :heavy_plus_sign: Feat(user/service) : UserService 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/user/service/UserService.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/user/service/UserService.java
@@ -1,5 +1,23 @@
 package com.sehbeomschool.nova.domain.user.service;
 
+import com.sehbeomschool.nova.domain.user.domain.User;
+import com.sehbeomschool.nova.domain.user.dto.KakaoUserInfoDto;
+import com.sehbeomschool.nova.domain.user.dto.UserResponseDto.LoginResponseDto;
+
 public interface UserService {
 
+    Long createUser(KakaoUserInfoDto user);
+
+    User readUser(Long userId);
+    User readUserBySocialId(Long socialId);
+
+    void updateUserProfileImg(Long userId, String profileImg);
+
+    void updateUserName(Long userId, String name);
+
+    void deleteUser(Long userId);
+
+    boolean isExistUser(Long socialId);
+
+    LoginResponseDto login(Long userId);
 }
