ae16397994acc2e1b70520a48459a32a1fc2a820
김연재 <yjkim9195@gmail.com>
2023-09-22 16:39:13 +0900

    :hammer: Refactor(Navigation/stack): 테스트 코드 삭제

--- frontend/src/navigation/Stack.js
+++ frontend/src/navigation/Stack.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Login, Main, Game } from '../screens'
-import RealEstatePage from "../screens/RealEstatePage";
 
 
 
@@ -10,7 +9,6 @@ const RootStack = createStackNavigator();
 const RootStackNavigator = () => {
   return (
     <RootStack.Navigator headerMode="none">
-      <RootStack.Screen name="RealEstatePage" component={RealEstatePage}/>
       <RootStack.Screen name="Login" component={Login}/>
       <RootStack.Screen name="Main" component={Main}/>
       <RootStack.Screen name="Game" component={Game}/>
