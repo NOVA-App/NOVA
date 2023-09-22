9a79b2556a6612952f9fb348d49bcac99650a800
김연재 <yjkim9195@gmail.com>
2023-09-22 16:37:28 +0900

    :heavy_plus_sign: Feat(page/realEstate): 부동산 탭 구현, 내 부동산 컴포 결합

--- frontend/src/navigation/Stack.js
+++ frontend/src/navigation/Stack.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Login, Main, Game } from '../screens'
+import RealEstatePage from "../screens/RealEstatePage";
+
 
 
 const RootStack = createStackNavigator();
@@ -8,6 +10,7 @@ const RootStack = createStackNavigator();
 const RootStackNavigator = () => {
   return (
     <RootStack.Navigator headerMode="none">
+      <RootStack.Screen name="RealEstatePage" component={RealEstatePage}/>
       <RootStack.Screen name="Login" component={Login}/>
       <RootStack.Screen name="Main" component={Main}/>
       <RootStack.Screen name="Game" component={Game}/>
