402ac44116d84c91126d62a91a3cafe66de56279
김연재 <yjkim9195@gmail.com>
2023-09-22 16:37:28 +0900

    :heavy_plus_sign: Feat(page/realEstate): 부동산 탭 구현, 내 부동산 컴포 결합

--- frontend/src/screens/RealEstatePage/TabSelect.js
+++ frontend/src/screens/RealEstatePage/TabSelect.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Modal, Dimensions, Text, StyleSheet, View } from 'react-native';
-import AAA from './a';
+import MyRealEstate from '../../components/MyRealEstateComponent';
 import BBB from './b';
 import CCC from './c';
 
 const TabSelect = ({selectedTab}) => {
   return (
     <View>
-      {selectedTab == 0 && <AAA/>}
+      {selectedTab == 0 && <MyRealEstate/>}
       {selectedTab == 1 && <BBB/>}
       {selectedTab == 2 && <CCC/>}
     </View>
