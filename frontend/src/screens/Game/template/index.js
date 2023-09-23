1ce873d1b7911e7f7e6fccd1635dee9c2de202a8
suchshin <scshin97@gmail.com>
2023-09-23 21:11:13 +0900

    :heavy_plus_sign: Feat(game/newspage): 하단 네브바에 뉴스 페이지 추가 및 연결

--- frontend/src/screens/Game/template/index.js
+++ frontend/src/screens/Game/template/index.js
@@ -1,6 +1,7 @@
 import {GameMainPage} from "./GameMainPage";
 import {Banking} from "./Banking";
 import { RealEstatePage } from "./RealEstatePage";
+import NewsPage from "./NewsPage";
 
-export { GameMainPage, Banking, RealEstatePage };
+export { GameMainPage, Banking, RealEstatePage, NewsPage };
 
