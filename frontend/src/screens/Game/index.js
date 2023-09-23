3f2183794e37e0b476e35d71d8a71f41300951d2
seokchul <scshin97@gmail.com>
2023-09-23 20:04:49 +0900

    :heavy_plus_sign: Feat(gamepage/bottomNavbarProperty): 게임 페이지 하단 네브바에 부동산 연결

--- frontend/src/screens/Game/index.js
+++ frontend/src/screens/Game/index.js
@@ -2,16 +2,17 @@ import React from "react";
 import { Image } from 'react-native';
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { GameMainPage, Banking } from "./template";
+import { GameMainPage, Banking, RealEstatePage } from "./template";
 
 const GameStack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function BottomNav() {
   return(
-    <Tab.Navigator>
+    <Tab.Navigator initialRouteName="GameMainPage">
       <Tab.Screen name="Banking" component={Banking} options={{
         tabBarLabel: '뱅킹',
+        headerShown: false,
         tabBarIcon: ({ focused, color, size }) => (
           <Image
           source={focused ? require('../../assets/BottomNav/Finance.png') : require('../../assets/BottomNav/Finance.png')}
@@ -21,6 +22,7 @@ function BottomNav() {
         }}/>
         <Tab.Screen name="GameMainPage" component={GameMainPage} options={{
       tabBarLabel: 'Home',
+      headerShown: false,
       tabBarIcon: ({ focused, color, size }) => (
         <Image
           source={focused ? require('../../assets/BottomNav/Home.png') : require('../../assets/BottomNav/Home.png')}
@@ -28,9 +30,19 @@ function BottomNav() {
         />
       ),
     }}/>
+    <Tab.Screen name="RealEstatePage" component={RealEstatePage} options={{
+      tabBarLabel: '부동산',
+      headerShown: false,
+      tabBarIcon: ({ focused, color, size }) => (
+        <Image
+          source={focused ? require('../../assets/BottomNav/Property.png') : require('../../assets/BottomNav/Property.png')}
+          style={{ width: 30, height: 30 }}
+        />
+      ),
+    }}/>
       {/* <Tab.Screen name="NewsPage" component={NewsPage}/>
-      <Tab.Screen name="PropertyPage" component={PropertyPage}/>
       <Tab.Screen name="StockPage" component={StockPage}/> */}
+      {/* {{ headerShown: false }} */}
     </Tab.Navigator>
   )
 }
