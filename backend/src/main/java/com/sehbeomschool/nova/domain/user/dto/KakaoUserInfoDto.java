2d3137561c99bf6cc13003c0bc8c4882ac4a8c52
limzzum <chfhddl1234@naver.com>
2023-09-24 16:23:57 +0900

    :construction_worker: Feat(user) : test 위한 생성자 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/user/dto/KakaoUserInfoDto.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/user/dto/KakaoUserInfoDto.java
@@ -9,18 +9,27 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class KakaoUserInfoDto {
+
     private Long id;
     private String name;
     private String profileImg;
 
     @Builder
-    public KakaoUserInfoDto(Map info){
+    public KakaoUserInfoDto(Long id, String name, String profileImg) {
+        this.id = id;
+        this.name = name;
+        this.profileImg = profileImg;
+    }
+
+
+    @Builder
+    public KakaoUserInfoDto(Map<String, Object> info) {
         this.id = (Long) info.get("id");
-        this.name = String.valueOf(((Map)info.get("properties")).get("nickname"));
+        this.name = String.valueOf(((Map<?, ?>) info.get("properties")).get("nickname"));
         this.profileImg = String.valueOf(info.get("profile_image_url"));
     }
 
-    public User toEntity(){
+    public User toEntity() {
         return User.builder().socialId(id).name(name).profileImg(profileImg).build();
     }
 }
