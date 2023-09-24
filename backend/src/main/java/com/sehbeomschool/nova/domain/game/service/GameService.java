07543d42f90b337128e6f8dffd4910f76c922317
Sehbeom <j3beom@gmail.com>
2023-09-24 19:34:07 +0900

    :heavy_plus_sign: Feat(game): 현재 해 정보 api service 계층 구현
    - readCurrentYear() 메서드 구현

--- backend/src/main/java/com/sehbeomschool/nova/domain/game/service/GameService.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/game/service/GameService.java
@@ -2,6 +2,7 @@ package com.sehbeomschool.nova.domain.game.service;
 
 import com.sehbeomschool.nova.domain.game.dto.GameRequestDto.GameStartRequestDto;
 import com.sehbeomschool.nova.domain.game.dto.GameRequestDto.MarryRequestDto;
+import com.sehbeomschool.nova.domain.game.dto.GameResponseDto.CurrentYearResponseDto;
 import com.sehbeomschool.nova.domain.game.dto.GameResponseDto.FixedCostResponseDto;
 import com.sehbeomschool.nova.domain.game.dto.GameResponseDto.GameStartResponseDto;
 
@@ -9,6 +10,9 @@ public interface GameService {
 
     GameStartResponseDto createGame(GameStartRequestDto gameStartRequestDto);
 
+    CurrentYearResponseDto readCurrentYear(Long gameId);
+
     FixedCostResponseDto readFixedCost(Long gameId);
+
     void marry(MarryRequestDto marryRequestDto);
 }
--- backend/src/main/java/com/sehbeomschool/nova/domain/game/service/GameServiceImpl.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/game/service/GameServiceImpl.java
@@ -17,6 +17,7 @@ import com.sehbeomschool.nova.domain.game.domain.Game;
 import com.sehbeomschool.nova.domain.game.domain.MyAssets;
 import com.sehbeomschool.nova.domain.game.dto.GameRequestDto.GameStartRequestDto;
 import com.sehbeomschool.nova.domain.game.dto.GameRequestDto.MarryRequestDto;
+import com.sehbeomschool.nova.domain.game.dto.GameResponseDto.CurrentYearResponseDto;
 import com.sehbeomschool.nova.domain.game.dto.GameResponseDto.FixedCostResponseDto;
 import com.sehbeomschool.nova.domain.game.dto.GameResponseDto.GameStartResponseDto;
 import com.sehbeomschool.nova.domain.game.exception.GameNotFoundException;
@@ -62,6 +63,20 @@ public class GameServiceImpl implements GameService {
 
         return GameStartResponseDto.builder().gameId(game.getId()).build();
     }
+
+    @Override
+    public CurrentYearResponseDto readCurrentYear(Long gameId) {
+        Game game = gameRepository.findById(gameId)
+            .orElseThrow(() -> new GameNotFoundException(GAME_NOT_FOUND.getMessage()));
+
+        return CurrentYearResponseDto.builder()
+            .game(game)
+            .events(game.getEvents())
+            .myAssets(game.getMyAssets())
+            .annualCost(game.getAnnualCost())
+            .build();
+    }
+
 
     @Override
     public FixedCostResponseDto readFixedCost(Long gameId) {
