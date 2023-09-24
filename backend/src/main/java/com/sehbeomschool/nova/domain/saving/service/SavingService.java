e8b79fa3bda70074f278a3fb1260b4f8b0a3ab2a
limzzum <chfhddl1234@naver.com>
2023-09-25 01:08:27 +0900

    :heavy_plus_sign: Feat(saving) : 금융 현황 조회 서비스 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/saving/service/SavingService.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/saving/service/SavingService.java
@@ -1,5 +1,9 @@
 package com.sehbeomschool.nova.domain.saving.service;
 
+import com.sehbeomschool.nova.domain.saving.dto.SavingResponseDto.SavingInfoResponseDto;
+
 public interface SavingService {
+
+    SavingInfoResponseDto readSavingInfo(Long gameId);
 
 }
