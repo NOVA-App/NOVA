7b6f50ac0bf28908a98c5697771c6abb15f40b58
limzzum <chfhddl1234@naver.com>
2023-09-24 16:39:54 +0900

    :construction_worker: Feat(user/token) : 테스트 위한 사용자 추가 후 토큰 발급 api

--- backend/src/main/java/com/sehbeomschool/nova/domain/user/api/UserApiController.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/user/api/UserApiController.java
@@ -3,8 +3,11 @@ package com.sehbeomschool.nova.domain.user.api;
 
 import com.sehbeomschool.nova.domain.user.domain.CustomUserDetails;
 import com.sehbeomschool.nova.domain.user.domain.User;
+import com.sehbeomschool.nova.domain.user.dto.KakaoUserInfoDto;
 import com.sehbeomschool.nova.domain.user.dto.UserResponseDto.UserInfoResponseDto;
+import com.sehbeomschool.nova.domain.user.service.UserService;
 import com.sehbeomschool.nova.global.dto.ResponseDto;
+import com.sehbeomschool.nova.global.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -20,6 +23,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class UserApiController {
 
+    private final JwtUtil jwtUtil;
+    private final UserService userService;
+
     @GetMapping("")
     public ResponseEntity<ResponseDto<UserInfoResponseDto>> getUserInfo(
         @AuthenticationPrincipal CustomUserDetails authentication) {
@@ -28,6 +34,15 @@ public class UserApiController {
             HttpStatus.OK).body(ResponseDto.create("",
             UserInfoResponseDto.builder().name(user.getName()).profileImg(user.getProfileImg())
                 .build()));
+    }
+
+    @GetMapping("/token")
+    public ResponseEntity<String> token() {
+        KakaoUserInfoDto test1 = KakaoUserInfoDto.builder().id(123L).name("test1")
+            .profileImg("").build();
+        userService.createUser(test1);
+        String jwtToken = jwtUtil.createJwtToken(1L);
+        return ResponseEntity.ok(jwtToken);
     }
 
 }
