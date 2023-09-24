77b9992ca8c1d3f230dee6d392f13d5af4c26c51
limzzum <chfhddl1234@naver.com>
2023-09-25 01:43:09 +0900

    :heavy_plus_sign: Feat(saving) : 적금 가입 서비스 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/saving/service/SavingService.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/saving/service/SavingService.java
@@ -1,9 +1,12 @@
 package com.sehbeomschool.nova.domain.saving.service;
 
+import com.sehbeomschool.nova.domain.saving.dto.SavingRequestDto.AddInstallmentRequestDto;
 import com.sehbeomschool.nova.domain.saving.dto.SavingResponseDto.SavingInfoResponseDto;
 
 public interface SavingService {
 
     SavingInfoResponseDto readSavingInfo(Long gameId);
+
+    void createInstallment(AddInstallmentRequestDto addInstallmentRequestDto);
 
 }
