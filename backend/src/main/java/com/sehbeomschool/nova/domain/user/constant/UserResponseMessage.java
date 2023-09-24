61a2b4f0598986827e2c8a2411a584cc4b4d9f5a
limzzum <chfhddl1234@naver.com>
2023-09-24 22:29:37 +0900

    :heavy_plus_sign: Feat(user) : 프로필 사진 업로드 응답 메시지 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/user/constant/UserResponseMessage.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/user/constant/UserResponseMessage.java
@@ -12,7 +12,8 @@ public enum UserResponseMessage {
     CREATE_ACCESS_TOKEN("새로운 토큰이 발급되었습니다"),
     SUCCESS_GET_USER_INFO("유저 정보 조회 성공"),
     FAIL_GET_USER_INFO("유저 정보 조회 실패"),
-    NOT_VALID_REFRESH_TOKEN("유효하지 않은 토큰입니다");
+    NOT_VALID_REFRESH_TOKEN("유효하지 않은 토큰입니다"),
+    SUCCES_PROFILE_UPDATE("프로필 사진이 업데이트 되었습니다.");
 
 
     private final String message;
