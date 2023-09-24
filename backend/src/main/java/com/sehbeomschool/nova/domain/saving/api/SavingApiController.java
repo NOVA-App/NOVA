f46c601c69d6a8e28091b7d639076e193b4a44cb
limzzum <chfhddl1234@naver.com>
2023-09-25 01:05:07 +0900

    :heavy_plus_sign: Feat(saving) : 금융 현황 조회 api

--- backend/src/main/java/com/sehbeomschool/nova/domain/saving/api/SavingApiController.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/saving/api/SavingApiController.java
@@ -1,5 +1,31 @@
 package com.sehbeomschool.nova.domain.saving.api;
 
+import com.sehbeomschool.nova.domain.saving.constant.SavingResponseMessage;
+import com.sehbeomschool.nova.domain.saving.dto.SavingResponseDto.SavingInfoResponseDto;
+import com.sehbeomschool.nova.domain.saving.service.SavingService;
+import com.sehbeomschool.nova.global.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/saving")
 public class SavingApiController {
 
+    private final SavingService savingService;
+
+    @GetMapping("/{gameId}")
+    public ResponseEntity<ResponseDto<SavingInfoResponseDto>> getSavingInfo(
+        @PathVariable Long gameId) {
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+            ResponseDto.create(SavingResponseMessage.SUCCESS_GET_SAVING_INFO.getMessage(),
+                savingService.readSavingInfo(gameId)));
+
+    }
 }
