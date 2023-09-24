728ecb142549006bcb62e68c15d3f686fb1fb6c4
limzzum <chfhddl1234@naver.com>
2023-09-24 16:27:47 +0900

    :construction_worker: Feat(user) : 유저 정보 받아오기 api

--- backend/src/main/java/com/sehbeomschool/nova/domain/user/api/UserApiController.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/user/api/UserApiController.java
@@ -1,12 +1,15 @@
 package com.sehbeomschool.nova.domain.user.api;
 
 
+import com.sehbeomschool.nova.domain.user.domain.CustomUserDetails;
+import com.sehbeomschool.nova.domain.user.domain.User;
 import com.sehbeomschool.nova.domain.user.dto.UserResponseDto.UserInfoResponseDto;
 import com.sehbeomschool.nova.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,14 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
+@Slf4j
 public class UserApiController {
 
     @GetMapping("")
     public ResponseEntity<ResponseDto<UserInfoResponseDto>> getUserInfo(
-        Authentication authentication) {
-        String name = authentication.getName();
+        @AuthenticationPrincipal CustomUserDetails authentication) {
+        User user = authentication.getUser();
         return ResponseEntity.status(
             HttpStatus.OK).body(ResponseDto.create("",
-            UserInfoResponseDto.builder().name(name).profileImg("").build()));
+            UserInfoResponseDto.builder().name(user.getName()).profileImg(user.getProfileImg())
+                .build()));
     }
+
 }
