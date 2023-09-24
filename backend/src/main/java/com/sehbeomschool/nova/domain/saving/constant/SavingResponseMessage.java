e408a6d2f71d57bfc19f7b8288257b5700a2e342
limzzum <chfhddl1234@naver.com>
2023-09-25 01:42:57 +0900

    :heavy_plus_sign: Feat(saving) : 적금 가입 응답 메시지 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/saving/constant/SavingResponseMessage.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/saving/constant/SavingResponseMessage.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum SavingResponseMessage {
 
-    SUCCESS_GET_SAVING_INFO("금융 현황 조회 완료");
+    SUCCESS_GET_SAVING_INFO("금융 현황 조회 완료"),
+    SUCCESS_ADD_INSTALLMENT("적금 가입 완료");
 
     private final String message;
 }
