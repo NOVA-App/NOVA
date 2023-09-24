7678f4097ed7f798fe79e9f55a8dd418ba68e8af
limzzum <chfhddl1234@naver.com>
2023-09-25 01:08:54 +0900

    :heavy_plus_sign: Feat(saving) : 금융 현황 조회 서비스 구현

--- backend/src/main/java/com/sehbeomschool/nova/domain/saving/service/SavingServiceImpl.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/saving/service/SavingServiceImpl.java
@@ -1,5 +1,39 @@
 package com.sehbeomschool.nova.domain.saving.service;
 
-public class SavingServiceImpl {
+import com.sehbeomschool.nova.domain.game.dao.GameRepository;
+import com.sehbeomschool.nova.domain.game.domain.Game;
+import com.sehbeomschool.nova.domain.game.domain.MyAssets;
+import com.sehbeomschool.nova.domain.saving.dao.SavingRepository;
+import com.sehbeomschool.nova.domain.saving.domain.InstallmentSavings;
+import com.sehbeomschool.nova.domain.saving.dto.SavingResponseDto.InstallmentSavingsDto;
+import com.sehbeomschool.nova.domain.saving.dto.SavingResponseDto.SavingInfoResponseDto;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class SavingServiceImpl implements SavingService {
+
+    private final SavingRepository savingRepository;
+    private final GameRepository gameRepository;
+
+    @Override
+    public SavingInfoResponseDto readSavingInfo(Long gameId) {
+        List<InstallmentSavings> installmentSavings = savingRepository.findByGameId(gameId)
+            .orElseThrow();
+
+        List<InstallmentSavingsDto> installmentSavingsDtos = installmentSavings.stream()
+            .map(InstallmentSavingsDto::new).toList();
+
+        Game game = gameRepository.findById(gameId).orElseThrow();
+        MyAssets myAssets = game.getMyAssets();
+
+        return SavingInfoResponseDto.builder().installmentSavings(installmentSavingsDtos)
+            .IRPCost(myAssets.getIRPAsset())
+            .build();
+
+    }
 }
