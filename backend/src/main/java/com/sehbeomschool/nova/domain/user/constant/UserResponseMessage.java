05fd5f6bcad55fbf0f4032da55e311541c51ea5d
limzzum <chfhddl1234@naver.com>
2023-09-24 22:13:51 +0900

    :heavy_plus_sign: Feat(user) : 응답 메시지 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/user/constant/UserResponseMessage.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/user/constant/UserResponseMessage.java
@@ -8,7 +8,12 @@ import lombok.RequiredArgsConstructor;
 public enum UserResponseMessage {
     LOGIN_SUCCESS("로그인 성공"),
     LOGIN_FAIL("로그인 실패"),
-    ACCESS_TOKEN_EXPIRED("토큰 만료됨");
+    ACCESS_TOKEN_EXPIRED("토큰 만료됨"),
+    CREATE_ACCESS_TOKEN("새로운 토큰이 발급되었습니다"),
+    SUCCESS_GET_USER_INFO("유저 정보 조회 성공"),
+    FAIL_GET_USER_INFO("유저 정보 조회 실패"),
+    NOT_VALID_REFRESH_TOKEN("유효하지 않은 토큰입니다");
+
 
     private final String message;
 
