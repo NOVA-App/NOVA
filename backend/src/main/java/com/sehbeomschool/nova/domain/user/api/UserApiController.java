3c1b171a80ed91ffee2c0776f462dade90d5d3dd
limzzum <chfhddl1234@naver.com>
2023-09-24 22:11:51 +0900

    :heavy_plus_sign: Feat(user) : 토큰 재발급 api

--- backend/src/main/java/com/sehbeomschool/nova/domain/user/api/UserApiController.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/user/api/UserApiController.java
@@ -1,9 +1,13 @@
 package com.sehbeomschool.nova.domain.user.api;
 
 
+import static com.sehbeomschool.nova.domain.user.constant.UserResponseMessage.CREATE_ACCESS_TOKEN;
+import static com.sehbeomschool.nova.domain.user.constant.UserResponseMessage.SUCCESS_GET_USER_INFO;
+
 import com.sehbeomschool.nova.domain.user.domain.CustomUserDetails;
 import com.sehbeomschool.nova.domain.user.domain.User;
 import com.sehbeomschool.nova.domain.user.dto.KakaoUserInfoDto;
+import com.sehbeomschool.nova.domain.user.dto.UserResponseDto.RefreshTokenResponseDto;
 import com.sehbeomschool.nova.domain.user.dto.UserResponseDto.UserInfoResponseDto;
 import com.sehbeomschool.nova.domain.user.service.UserService;
 import com.sehbeomschool.nova.global.dto.ResponseDto;
@@ -14,6 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,7 +36,7 @@ public class UserApiController {
         @AuthenticationPrincipal CustomUserDetails authentication) {
         User user = authentication.getUser();
         return ResponseEntity.status(
-            HttpStatus.OK).body(ResponseDto.create("",
+            HttpStatus.OK).body(ResponseDto.create(SUCCESS_GET_USER_INFO.getMessage(),
             UserInfoResponseDto.builder().name(user.getName()).profileImg(user.getProfileImg())
                 .build()));
     }
@@ -43,6 +48,16 @@ public class UserApiController {
         userService.createUser(test1);
         String jwtToken = jwtUtil.createJwtToken(1L);
         return ResponseEntity.ok(jwtToken);
+    }
+
+    @GetMapping("/refreshtoken")
+    public ResponseEntity<ResponseDto<RefreshTokenResponseDto>> getUserInfo(
+        @RequestBody String refreshToken) {
+        String accessToken = jwtUtil.reCreateJwtToken(refreshToken);
+        return ResponseEntity.status(
+            HttpStatus.OK).body(ResponseDto.create(CREATE_ACCESS_TOKEN.getMessage(),
+            RefreshTokenResponseDto.builder().accessToken(accessToken)
+                .build()));
     }
 
 }
