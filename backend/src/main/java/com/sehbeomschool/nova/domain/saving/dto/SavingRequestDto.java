f58d1f1208a4e9b6cfd76f9dcf6c0657858ef4b8
limzzum <chfhddl1234@naver.com>
2023-09-25 01:42:45 +0900

    :heavy_plus_sign: Feat(saving) : 적금 가입 요청 dto 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/saving/dto/SavingRequestDto.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/saving/dto/SavingRequestDto.java
@@ -1,5 +1,30 @@
 package com.sehbeomschool.nova.domain.saving.dto;
 
+import com.sehbeomschool.nova.domain.game.domain.Game;
+import com.sehbeomschool.nova.domain.saving.domain.InsInterest;
+import com.sehbeomschool.nova.domain.saving.domain.InstallmentSavings;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 public class SavingRequestDto {
 
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class AddInstallmentRequestDto {
+
+        private Long gameId;
+        private String name;
+        private int period;
+        private Long amount;
+
+        public InstallmentSavings toEntity(Game game, InsInterest insInterest) {
+            return InstallmentSavings.builder().game(game).name(name).amount(amount)
+                .totalAmount(amount)
+                .interest(insInterest).startAge(game.getCurrentAge())
+                .endAge(game.getCurrentAge() + period).build();
+        }
+
+    }
 }
