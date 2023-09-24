f8e52bf446e0b8eb913b8cc0958dbdc4f91e4e5d
limzzum <chfhddl1234@naver.com>
2023-09-24 22:16:35 +0900

    :hammer: Refactor(user) : 사진 업데이트 메서드 매개변수 타입 변경

--- backend/src/main/java/com/sehbeomschool/nova/domain/user/service/UserService.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/user/service/UserService.java
@@ -3,15 +3,17 @@ package com.sehbeomschool.nova.domain.user.service;
 import com.sehbeomschool.nova.domain.user.domain.User;
 import com.sehbeomschool.nova.domain.user.dto.KakaoUserInfoDto;
 import com.sehbeomschool.nova.domain.user.dto.UserResponseDto.LoginResponseDto;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface UserService {
 
     Long createUser(KakaoUserInfoDto user);
 
     User readUser(Long userId);
+
     User readUserBySocialId(Long socialId);
 
-    void updateUserProfileImg(Long userId, String profileImg);
+    void updateUserProfileImg(Long userId, MultipartFile profileImg);
 
     void updateUserName(Long userId, String name);
 
