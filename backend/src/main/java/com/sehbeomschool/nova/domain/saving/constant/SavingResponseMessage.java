a1d64fb9cf12403d32adc0ecca01201dc5434c76
limzzum <chfhddl1234@naver.com>
2023-09-25 01:08:10 +0900

    :heavy_plus_sign: Feat(saving) : 금융 현황 조회 응답 메시지 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/saving/constant/SavingResponseMessage.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/saving/constant/SavingResponseMessage.java
@@ -1,4 +1,13 @@
 package com.sehbeomschool.nova.domain.saving.constant;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public enum SavingResponseMessage {
+
+    SUCCESS_GET_SAVING_INFO("금융 현황 조회 완료");
+
+    private final String message;
 }
