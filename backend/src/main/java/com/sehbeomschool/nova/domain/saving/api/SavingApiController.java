9addc45c875b2a912856b97a376e79b3a92c6d91
limzzum <chfhddl1234@naver.com>
2023-09-25 01:42:28 +0900

    :heavy_plus_sign: Feat(saving) : 적금 가입 api

--- backend/src/main/java/com/sehbeomschool/nova/domain/saving/api/SavingApiController.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/saving/api/SavingApiController.java
@@ -1,6 +1,9 @@
 package com.sehbeomschool.nova.domain.saving.api;
 
-import com.sehbeomschool.nova.domain.saving.constant.SavingResponseMessage;
+import static com.sehbeomschool.nova.domain.saving.constant.SavingResponseMessage.SUCCESS_ADD_INSTALLMENT;
+import static com.sehbeomschool.nova.domain.saving.constant.SavingResponseMessage.SUCCESS_GET_SAVING_INFO;
+
+import com.sehbeomschool.nova.domain.saving.dto.SavingRequestDto.AddInstallmentRequestDto;
 import com.sehbeomschool.nova.domain.saving.dto.SavingResponseDto.SavingInfoResponseDto;
 import com.sehbeomschool.nova.domain.saving.service.SavingService;
 import com.sehbeomschool.nova.global.dto.ResponseDto;
@@ -9,6 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,8 +29,19 @@ public class SavingApiController {
         @PathVariable Long gameId) {
 
         return ResponseEntity.status(HttpStatus.OK).body(
-            ResponseDto.create(SavingResponseMessage.SUCCESS_GET_SAVING_INFO.getMessage(),
+            ResponseDto.create(SUCCESS_GET_SAVING_INFO.getMessage(),
                 savingService.readSavingInfo(gameId)));
+
+    }
+
+    @PostMapping("")
+    public ResponseEntity<ResponseDto<SavingInfoResponseDto>> addInstallment(
+        @RequestBody AddInstallmentRequestDto addInstallmentRequestDto) {
+
+        savingService.createInstallment(addInstallmentRequestDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+            ResponseDto.create(SUCCESS_ADD_INSTALLMENT.getMessage()));
 
     }
 }
