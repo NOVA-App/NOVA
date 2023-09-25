b677a8f28e3c36b8eca64752e8670113cab1dd03
임대영 <ldy092000@gmail.com>
2023-09-25 14:31:20 +0900

    :hammer: Refact(news/repo): enum 타입 최신화

--- backend/src/main/java/com/sehbeomschool/nova/domain/news/dao/RealtyNewsRepository.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/news/dao/RealtyNewsRepository.java
@@ -1,5 +1,6 @@
 package com.sehbeomschool.nova.domain.news.dao;
 
+import com.sehbeomschool.nova.domain.news.domain.Prediction;
 import com.sehbeomschool.nova.domain.news.domain.RealtyNews;
 import com.sehbeomschool.nova.domain.news.domain.StockNews;
 import io.lettuce.core.dynamic.annotation.Param;
@@ -8,5 +9,5 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface RealtyNewsRepository extends JpaRepository<RealtyNews, Long> {
     @Query(value = "SELECT * FROM REALTY_NEWS rn WHERE rn.REALTY_ID = :realtyId AND rn.PREDICTION = :prediction ORDER BY RAND() LIMIT 1", nativeQuery = true)
-    RealtyNews findRealtyNewsByRandom(@Param("realtyId") Long realtyId, @Param("prediction") String prediction);
+    RealtyNews findRealtyNewsByRandom(@Param("realtyId") Long realtyId, @Param("prediction") Prediction prediction);
 }
