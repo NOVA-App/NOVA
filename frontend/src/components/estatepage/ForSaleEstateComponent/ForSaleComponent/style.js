ae69a1c0a9a3930bd57f4973c2f0ed590aa36fa9
김의준 <euijun98@naver.com>
2023-09-25 14:34:13 +0900

    :heavy_plus_sign: Feat(estate/UI): 디테일 페이지 UI 구현

---
+++ frontend/src/components/estatepage/ForSaleEstateComponent/ForSaleComponent/style.js
@@ -0,0 +1,51 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  background-color: white;
+  width: 100%;
+  height: 98%;
+  border-radius: 10px;
+  margin-bottom: 1%;
+`;
+
+export const TagContainer = styled.View`
+  background-color: #038c7f;
+  width: 50%;
+  height: 100%;
+  justify-items: center;
+  align-items: center;
+  border-radius: 10px;
+  margin-top: 3%;
+`;
+
+export const TotalAssetContainer = styled.View`
+  background-color: transparent;
+  width: 90%;
+  justify-items: center;
+  align-items: center;
+  border-radius: 10px;
+  margin-left: 5%;
+  margin-top: 3%;
+  padding-top: 3%;
+`;
+
+export const MiddleText = styled.Text`
+  font-size: 16px;
+`;
+
+export const CenterView = styled.View`
+  justify-items: center;
+  width: 100%;
+`;
+
+export const ImgContainer = styled.Image`
+  width: 70%;
+  height: 40%;
+  border: 0.5px;
+  border-radius: 10px;
+`;
+
+export const InfoText = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+`;
