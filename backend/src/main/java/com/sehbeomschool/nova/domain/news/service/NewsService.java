8ab2903911d913a129a7b87de0c12edb3771cbc8
임대영 <ldy092000@gmail.com>
2023-09-25 13:57:35 +0900

    :heavy_plus_sign: Feat(stock/service): 뉴스 최신화를 위한 service 작성 및 new조회 수정

--- backend/src/main/java/com/sehbeomschool/nova/domain/news/service/NewsService.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/news/service/NewsService.java
@@ -1,8 +1,13 @@
 package com.sehbeomschool.nova.domain.news.service;
 
+import com.sehbeomschool.nova.domain.game.domain.Game;
 import com.sehbeomschool.nova.domain.news.domain.News;
 import java.util.List;
 
 public interface NewsService {
-    List<News> readNews(Long gameId);
+    List<String> readNews(Long gameId);
+
+    void createNewsInfoByGameStart(Game game, Long ageId);
+
+    void updateNewsInfoByNextYear(Long gameId, Long ageId);
 }
