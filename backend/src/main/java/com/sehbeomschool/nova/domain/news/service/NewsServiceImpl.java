9eafdf39bc246d0fb58138bc3bb40c30d8068a7f
임대영 <ldy092000@gmail.com>
2023-09-25 13:57:35 +0900

    :heavy_plus_sign: Feat(stock/service): 뉴스 최신화를 위한 service 작성 및 new조회 수정

--- backend/src/main/java/com/sehbeomschool/nova/domain/news/service/NewsServiceImpl.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/news/service/NewsServiceImpl.java
@@ -1,22 +1,98 @@
 package com.sehbeomschool.nova.domain.news.service;
 
+import com.sehbeomschool.nova.domain.game.domain.Game;
 import com.sehbeomschool.nova.domain.news.dao.NewsInfoRepository;
 import com.sehbeomschool.nova.domain.news.dao.NewsRepository;
+import com.sehbeomschool.nova.domain.news.dao.RealtyNewsRepository;
+import com.sehbeomschool.nova.domain.news.dao.StockNewsRepository;
 import com.sehbeomschool.nova.domain.news.domain.News;
+import com.sehbeomschool.nova.domain.news.domain.NewsInfo;
+import com.sehbeomschool.nova.domain.realty.dao.RealtyInfoRepository;
+import com.sehbeomschool.nova.domain.realty.domain.RealtyInfo;
+import com.sehbeomschool.nova.domain.stock.dao.StocksInfoRepository;
+import com.sehbeomschool.nova.domain.stock.domain.StocksInfo;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class NewsServiceImpl implements NewsService{
+public class NewsServiceImpl implements NewsService {
 
     private final NewsRepository newsRepository;
     private final NewsInfoRepository newsInfoRepository;
+    private final RealtyNewsRepository realtyNewsRepository;
+    private final StockNewsRepository stockNewsRepository;
+    private final RealtyInfoRepository realtyInfoRepository;
+    private final StocksInfoRepository stocksInfoRepository;
 
 
-    public List<News> readNews(Long gameId) {
-        return newsInfoRepository.findByGameId(gameId);
+    public List<String> readNews(Long gameId) {
+        return newsInfoRepository.findContentByGameId(gameId);
     }
+
+    @Override
+    @Transactional
+    public void createNewsInfoByGameStart(Game game, Long ageId) {
+        List<StocksInfo> stocksInfos = stocksInfoRepository.findStocksInfosByRandom(ageId);
+        List<RealtyInfo> realtyInfos = realtyInfoRepository.findRealtyInfosByRandom(game.getId());
+
+        List<News> list = new ArrayList<>();
+
+        for (StocksInfo si : stocksInfos) {
+            String prediction = (si.getNextPrice() - si.getCurrentPrice()) > 0 ? "GOOD" : "BAD";
+            list.add(stockNewsRepository.findStockNewsByRandom(si.getStock().getId(), prediction));
+        }
+
+        for (RealtyInfo ri : realtyInfos) {
+            String prediction = (ri.getNextPrice() - ri.getCurrentPrice()) > 0 ? "GOOD" : "BAD";
+            list.add(realtyNewsRepository.findRealtyNewsByRandom(ri.getRealty().getId(),
+                prediction));
+        }
+
+        Collections.shuffle(list);
+
+        for (News n : list) {
+            NewsInfo newsInfo = NewsInfo.builder()
+                .id(null)
+                .game(game)
+                .news(n)
+                .build();
+
+            newsInfoRepository.save(newsInfo);
+        }
+    }
+
+    @Override
+    public void updateNewsInfoByNextYear(Long gameId, Long ageId) {
+        List<NewsInfo> list = newsInfoRepository.findNewsInfosByGameId(gameId);
+
+        List<StocksInfo> stocksInfos = stocksInfoRepository.findStocksInfosByRandom(ageId);
+        List<RealtyInfo> realtyInfos = realtyInfoRepository.findRealtyInfosByRandom(gameId);
+
+        List<News> news = new ArrayList<>();
+
+        for (StocksInfo si : stocksInfos) {
+            String prediction = (si.getNextPrice() - si.getCurrentPrice()) > 0 ? "GOOD" : "BAD";
+            news.add(stockNewsRepository.findStockNewsByRandom(si.getStock().getId(), prediction));
+        }
+
+        for (RealtyInfo ri : realtyInfos) {
+            String prediction = (ri.getNextPrice() - ri.getCurrentPrice()) > 0 ? "GOOD" : "BAD";
+            news.add(realtyNewsRepository.findRealtyNewsByRandom(ri.getRealty().getId(),
+                prediction));
+        }
+
+        Collections.shuffle(news);
+
+        for (int i = 0; i < list.size(); i++) {
+            NewsInfo ni = list.get(i);
+            ni.setNews(news.get(i));
+        }
+    }
+
+
 }
