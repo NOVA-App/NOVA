dadc29604a7378032fa24a1c6fb0be0a2fa21718
김연재 <yjkim9195@gmail.com>
2023-09-25 15:37:21 +0900

    :hammer: Refactor(page/banking): 코드 정리

--- frontend/src/navigation/Stack.js
+++ frontend/src/navigation/Stack.js
@@ -1,19 +1,15 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Login, Main, Game } from "../screens";
-import { Account } from "../screens/Game/template/Account";
-import FinancialProduct from "../screens/Game/template/FinancialProduct";
 
 const RootStack = createStackNavigator();
 
 const RootStackNavigator = () => {
   return (
     <RootStack.Navigator headerMode="none">
-      <RootStack.Screen name="Account" component={Account} />
-      <RootStack.Screen name="Game" component={Game} />
-      <RootStack.Screen name="FinancialProduct" component={FinancialProduct} />
       <RootStack.Screen name="Login" component={Login} />
       <RootStack.Screen name="Main" component={Main} />
+      <RootStack.Screen name="Game" component={Game} />
     </RootStack.Navigator>
   );
 };
