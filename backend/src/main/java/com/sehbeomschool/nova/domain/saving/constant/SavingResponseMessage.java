4bd7859d27dc402602f41f5e53df8ebe2d3aa1e2
임정현 <chfhddl1234@naver.com>
2023-09-25 11:18:01 +0900

    :heavy_plus_sign: Feat(saving) : 적금 해지 응답 메시지 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/saving/constant/SavingResponseMessage.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/saving/constant/SavingResponseMessage.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum SavingResponseMessage {
 
     SUCCESS_GET_SAVING_INFO("금융 현황 조회 완료"),
-    SUCCESS_ADD_INSTALLMENT("적금 가입 완료");
+    SUCCESS_ADD_INSTALLMENT("적금 가입 완료"),
+    SUCCESS_DELETE_INSTALLMENT("적금 해지 완료");
 
     private final String message;
 }
