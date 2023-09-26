ca6a4d23ee6f0b8fd7c295c112ec043e6305deb2
Sehbeom <j3beom@gmail.com>
2023-09-26 10:29:15 +0900

    :hammer: Refactor(game): Game 시작 부동산 및 주식 생성 리팩토링
    - 파라미터 객체로 받아오도록 수정
    - Prediction 파라미터 Enum에서 String으로 수정

--- backend/src/main/java/com/sehbeomschool/nova/domain/news/service/NewsService.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/news/service/NewsService.java
@@ -1,5 +1,6 @@
 package com.sehbeomschool.nova.domain.news.service;
 
+import com.sehbeomschool.nova.domain.game.domain.Ages;
 import com.sehbeomschool.nova.domain.game.domain.Game;
 import com.sehbeomschool.nova.domain.news.dto.NewsResponseDto.ReadNewsResponseDto;
 
@@ -7,7 +8,7 @@ public interface NewsService {
 
     ReadNewsResponseDto readNews(Long gameId);
 
-    void createNewsInfoByGameStart(Game game, Long ageId);
+    void createNewsInfoByGameStart(Game game, Ages age);
 
-    void updateNewsInfoByNextYear(Long gameId, Long ageId);
+    void updateNewsInfoByNextYear(Game game, Ages age);
 }
