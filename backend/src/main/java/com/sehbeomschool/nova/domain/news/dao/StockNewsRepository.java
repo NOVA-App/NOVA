16e5e1a3d63eeed1e59f66a9f32ce9400a213cae
Sehbeom <j3beom@gmail.com>
2023-09-26 10:27:19 +0900

    :bug: Fix(game): Game 시작 부동산 및 주식 생성 오류 해결
    - @Param 어노테이션 수정
    - SQL 수정

--- backend/src/main/java/com/sehbeomschool/nova/domain/news/dao/StockNewsRepository.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/news/dao/StockNewsRepository.java
@@ -1,14 +1,17 @@
 package com.sehbeomschool.nova.domain.news.dao;
 
-import com.sehbeomschool.nova.domain.news.domain.Prediction;
 import com.sehbeomschool.nova.domain.news.domain.StockNews;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StockNewsRepository extends JpaRepository<StockNews, Long> {
 
-    @Query(value = "SELECT * FROM STOCK_NEWS sn WHERE sn.STOCK_ID = :stockId AND sn.PREDICTION = :prediction ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM STOCK_NEWS sn, NEWS n "
+        + "WHERE sn.NEWS_ID = n.NEWS_ID AND "
+        + "sn.STOCK_ID = :stockId AND "
+        + "n.PREDICTION = :prediction "
+        + "ORDER BY RAND() LIMIT 1", nativeQuery = true)
     StockNews findStockNewsByRandom(@Param("stockId") Long stockId,
-        @Param("prediction") Prediction prediction);
+        @Param("prediction") String prediction);
 }
