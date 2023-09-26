781cc65d4ccd24a55f2164338d67e08b55ae6a87
임대영 <ldy092000@gmail.com>
2023-09-26 11:13:19 +0900

    :bug: Fix(Stock/repo): Query 수정

--- backend/src/main/java/com/sehbeomschool/nova/domain/stock/dao/StocksInfoRepository.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/stock/dao/StocksInfoRepository.java
@@ -10,7 +10,7 @@ public interface StocksInfoRepository extends JpaRepository<StocksInfo, Long> {
 
     List<StocksInfo> findStocksInfosByAgeId(Long ageId);
 
-    @Query("SELECT si.currentPrice FROM StocksInfo si WHERE si.id IN (SELECT a.id FROM Ages a WHERE a.game.id = :gameId) AND si.stock.id = :stockId")
+    @Query("SELECT si.currentPrice FROM StocksInfo si WHERE si.age.id IN (SELECT a.id FROM Ages a WHERE a.game.id = :gameId) AND si.stock.id = :stockId")
     List<Long> findCurrentPricesByGameIdAndStockId(@Param("gameId") Long gameId,
         @Param("stockId") Long stockId);
 
