f8bb2b85a03527f550066df25dce444e6f2e5c76
김의준 <euijun98@naver.com>
2023-09-26 14:24:16 +0900

    :heavy_plus_sign: Feat(ranking/UI) : 랭킹페이지 UI 구현

---
+++ frontend/src/components/rankingpage/RankBox/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  border-width: 0.5px;
+  border-color: black;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  background-color: white;
+  width: 95%;
+  flex-direction: row;
+  margin-left: 2.5%;
+  margin-top: 1%;
+  margin-bottom: 1%;
+  padding: 1%;
+  box-shadow: 10px 5px 5px black;
+`;
