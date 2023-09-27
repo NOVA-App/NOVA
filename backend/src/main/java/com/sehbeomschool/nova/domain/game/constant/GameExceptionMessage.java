2259b2e88d615e8b01a80f8665783ae63d076889
Sehbeom <j3beom@gmail.com>
2023-09-27 11:00:38 +0900

    :heavy_plus_sign: Feat(game): 여유 자금 부족 예외 추가
    - 여유 자금 부족 상황에 대한 예외 UsableAssetNotEnoughException 추가
    - 예외 메세지 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/game/constant/GameExceptionMessage.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/game/constant/GameExceptionMessage.java
@@ -1,7 +1,8 @@
 package com.sehbeomschool.nova.domain.game.constant;
 
 public enum GameExceptionMessage {
-    GAME_NOT_FOUND("해당 게임 미존재");
+    GAME_NOT_FOUND("해당 게임 미존재"),
+    USABLE_ASSET_NOT_ENOUGH("여유 자금 부족");
 
     private final String message;
 
