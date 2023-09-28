7347b3248f765f374312d088f1471ecae6e6981d
limzzum <chfhddl1234@naver.com>
2023-09-28 19:01:07 +0900

    :heavy_plus_sign: Feat(saving) : IRP 이자 계산 추가

--- backend/src/main/java/com/sehbeomschool/nova/global/util/RandomCalculator.java
+++ backend/src/main/java/com/sehbeomschool/nova/global/util/RandomCalculator.java
@@ -22,4 +22,18 @@ public class RandomCalculator {
     public static Long calStock(Long price) {
         return price * (100L + calPercent(50, 25)) / 100;
     }
+
+
+    private static Long calPercent(int min, int max, int bias) {
+        Long val;
+        do {
+            val = (long) (random.nextGaussian() * bias);
+        } while (val > max || val < min);
+
+        return val;
+    }
+
+    public static Long calIrp(Long price) {
+        return price * (calPercent(-1, 10, 5)) / 100;
+    }
 }
