66589b163c9ff4bc0b132745efea976113a7ae5e
limzzum <chfhddl1234@naver.com>
2023-09-28 19:16:41 +0900

    :heavy_plus_sign: Feat(saving) : IRP 납입금 수정 응답 메시지 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/saving/constant/SavingResponseMessage.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/saving/constant/SavingResponseMessage.java
@@ -9,7 +9,8 @@ public enum SavingResponseMessage {
 
     SUCCESS_GET_SAVING_INFO("금융 현황 조회 완료"),
     SUCCESS_ADD_INSTALLMENT("적금 가입 완료"),
-    SUCCESS_DELETE_INSTALLMENT("적금 해지 완료");
+    SUCCESS_DELETE_INSTALLMENT("적금 해지 완료"),
+    SUCCESS_UPDATE_IRP("IRP 납입금 수정 완료");
 
     private final String message;
 }
