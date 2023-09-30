7f1c0e4f9233748351fb9a8a115594c13666c28c
임대영 <ldy092000@gmail.com>
2023-09-30 15:11:06 +0900

    :heavy_plus_sign: Feat(realty/api):대출 조회, 상환 컨트롤러 작성

--- backend/src/main/java/com/sehbeomschool/nova/domain/realty/api/RealtyApiController.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/realty/api/RealtyApiController.java
@@ -1,11 +1,15 @@
 package com.sehbeomschool.nova.domain.realty.api;
 
 import static com.sehbeomschool.nova.domain.realty.constant.RealtyResponseMessage.BUY_REALTY;
+import static com.sehbeomschool.nova.domain.realty.constant.RealtyResponseMessage.READ_LOAN;
 import static com.sehbeomschool.nova.domain.realty.constant.RealtyResponseMessage.READ_REALTY;
+import static com.sehbeomschool.nova.domain.realty.constant.RealtyResponseMessage.REPAYMENT_LOAN;
 import static com.sehbeomschool.nova.domain.realty.constant.RealtyResponseMessage.SELL_REALTY;
 
 import com.sehbeomschool.nova.domain.realty.constant.RealtyResponseMessage;
+import com.sehbeomschool.nova.domain.realty.dto.RealtyRequestDto.RepaymentLoanRequestDto;
 import com.sehbeomschool.nova.domain.realty.dto.RealtyRequestDto.TradeRealtyRequestDto;
+import com.sehbeomschool.nova.domain.realty.dto.RealtyResponseDto.ReadLoanListResponseDto;
 import com.sehbeomschool.nova.domain.realty.dto.RealtyResponseDto.ReadMyRealtyDetailResponseDto;
 import com.sehbeomschool.nova.domain.realty.dto.RealtyResponseDto.ReadMyRealtyResponseDto;
 import com.sehbeomschool.nova.domain.realty.dto.RealtyResponseDto.ReadRealtyDetailResponseDto;
@@ -14,11 +18,11 @@ import com.sehbeomschool.nova.domain.realty.service.RealtyService;
 import com.sehbeomschool.nova.global.dto.ResponseDto;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -91,12 +95,36 @@ public class RealtyApiController {
     }
 
     @DeleteMapping("/sell/{gameId}/{realtyId}")
-    public ResponseEntity<ResponseDto<?>> sellRealty(@PathVariable(value = "gameId") Long gameId, @PathVariable(value = "realtyId") Long realtyId){
+    public ResponseEntity<ResponseDto<?>> sellRealty(@PathVariable(value = "gameId") Long gameId,
+        @PathVariable(value = "realtyId") Long realtyId) {
         realtyService.sellRealty(gameId, realtyId);
 
         return ResponseEntity.status(HttpStatus.OK).body(
             ResponseDto.create(
                 SELL_REALTY.getMessage()
+            )
+        );
+    }
+
+    @GetMapping("/loan/{gameId}")
+    public ResponseEntity<ResponseDto<List<ReadLoanListResponseDto>>> readLoan(
+        @PathVariable(value = "gameId") Long gameId) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+            ResponseDto.create(
+                READ_LOAN.getMessage(),
+                realtyService.readLoan(gameId)
+            )
+        );
+    }
+
+    @PatchMapping("/loan/repayment")
+    public ResponseEntity<ResponseDto<?>> repaymentLoan(
+        @RequestBody RepaymentLoanRequestDto repaymentLoanRequestDto) {
+        realtyService.repaymentLoan(repaymentLoanRequestDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+            ResponseDto.create(
+                REPAYMENT_LOAN.getMessage()
             )
         );
     }
