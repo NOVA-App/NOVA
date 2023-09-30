0ebc0ece7c6a90b827736365abb60aebcc234749
Sehbeom <j3beom@gmail.com>
2023-10-01 05:25:27 +0900

    :heavy_plus_sign: Feat(game): 랭킹 목록 조회 dao 메서드 추가
    - 종료된 게임의 자산 성장률 기준 내림 차순 조회 메서드 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/game/dao/GameRepository.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/game/dao/GameRepository.java
@@ -3,6 +3,7 @@ package com.sehbeomschool.nova.domain.game.dao;
 import com.sehbeomschool.nova.domain.game.domain.Game;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +17,8 @@ public interface GameRepository extends JpaRepository<Game, Long> {
     @EntityGraph(attributePaths = {"myAssets", "annualAsset"})
     @Query("SELECT g FROM Game g WHERE g.user.id = :userId AND g.currentAge = 60")
     List<Game> findFinishedGameByUserId(@Param("userId") Long userId);
+
+    @EntityGraph(attributePaths = {"user"})
+    @Query("SELECT g FROM Game g WHERE g.currentAge = 60")
+    List<Game> findRankList(Pageable pageable);
 }
