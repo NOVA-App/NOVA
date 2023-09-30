61d4ec53a3aeb394a18d5ac528ac5848365c98a6
임대영 <ldy092000@naver.com>
2023-09-30 15:11:35 +0900

    :heavy_plus_sign: Feat(realty/api):대출 조회, 상환 response 메세지 작성

--- backend/src/main/java/com/sehbeomschool/nova/domain/realty/constant/RealtyResponseMessage.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/realty/constant/RealtyResponseMessage.java
@@ -11,7 +11,9 @@ public enum RealtyResponseMessage {
     READ_REALTIES("부동산 목록 반환 완료"),
     READ_REALTY("부동산 매물 상세 정보 반환 완료"),
     BUY_REALTY("부동산 매수 완료"),
-    SELL_REALTY("부동산 매도 완료");
+    SELL_REALTY("부동산 매도 완료"),
+    READ_LOAN("대출 목록 반환 완료"),
+    REPAYMENT_LOAN("대출 상환 완료");
 
     private final String message;
 }
