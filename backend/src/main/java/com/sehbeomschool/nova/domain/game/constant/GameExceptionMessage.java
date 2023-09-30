a418fbb0f213efab840c5e4c4df37d168b7fde46
Sehbeom <j3beom@gmail.com>
2023-10-01 05:43:58 +0900

    :heavy_plus_sign: Feat(game): 진행 중 게임 조회 예외 메세지 추가
    - 진행 중인 게임이 없는 경우에 대한 예외 응답 메세지 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/game/constant/GameExceptionMessage.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/game/constant/GameExceptionMessage.java
@@ -2,6 +2,7 @@ package com.sehbeomschool.nova.domain.game.constant;
 
 public enum GameExceptionMessage {
     GAME_NOT_FOUND("해당 게임 미존재"),
+    IN_PROGRESS_GAME_NOT_FOUND("진행 중 게임 조회 실패"),
     USABLE_ASSET_NOT_ENOUGH("여유 자금 부족");
 
     private final String message;
