7310360a06dfcd974fd061981f87dae923fe62e1
Sehbeom <j3beom@gmail.com>
2023-10-01 04:46:51 +0900

    :heavy_plus_sign: Feat(game): 종료된 Game 조회 메서드 추가
    - 특정 user의 종료된 Game 목록 조회 메서드 추가

--- backend/src/main/java/com/sehbeomschool/nova/domain/game/dao/GameRepository.java
+++ backend/src/main/java/com/sehbeomschool/nova/domain/game/dao/GameRepository.java
@@ -1,12 +1,19 @@
 package com.sehbeomschool.nova.domain.game.dao;
 
 import com.sehbeomschool.nova.domain.game.domain.Game;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
 
     @EntityGraph(attributePaths = {"myAssets", "annualAsset"})
     Optional<Game> findById(Long gameId);
+
+    @EntityGraph(attributePaths = {"myAssets", "annualAsset"})
+    @Query("SELECT g FROM Game g WHERE g.user.id = :userId AND g.currentAge = 60")
+    List<Game> findFinishedGameByUserId(@Param("userId") Long userId);
 }
