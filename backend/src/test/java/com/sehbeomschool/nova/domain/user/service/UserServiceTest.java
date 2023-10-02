77ad3872143133c6195e2c74daf72dbc943b296b
limzzum <chfhddl1234@naver.com>
2023-10-02 22:33:17 +0900

    :white_check_mark: Test(user) : 회원 생성 실패 테스트

--- backend/src/test/java/com/sehbeomschool/nova/domain/user/service/UserServiceTest.java
+++ backend/src/test/java/com/sehbeomschool/nova/domain/user/service/UserServiceTest.java
@@ -1,13 +1,16 @@
 package com.sehbeomschool.nova.domain.user.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
 import com.sehbeomschool.nova.domain.user.dao.UserRepository;
 import com.sehbeomschool.nova.domain.user.domain.User;
+import com.sehbeomschool.nova.domain.user.exception.UserExistException;
 import com.sehbeomschool.nova.global.file.FileStore;
 import com.sehbeomschool.nova.global.util.JwtUtil;
-import org.junit.jupiter.api.Assertions;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -31,15 +34,27 @@ class UserServiceTest {
     private JwtUtil jwtUtil;
 
     @Test
-    @DisplayName("회원 생성 테스트")
-    void createUser() {
+    @DisplayName("회원 생성 성공 테스트")
+    void success_createUser() {
         User user = User.builder().id(1L).build();
         given(userRepository.save(user)).willReturn(user);
 
         Long userId = userService.createUser(user);
 
         then(userRepository).should().save(user);
-        Assertions.assertEquals(userId, 1L);
+        assertThat(userId).isEqualTo(1L);
+
+    }
+
+    @Test
+    @DisplayName("회원 생성 실패 테스트")
+    void fail_createUser() {
+
+        User user = User.builder().id(1L).socialId(123L).build();
+        given(userRepository.findBySocialId(user.getSocialId())).willReturn(Optional.of(user));
+
+        assertThatThrownBy(() -> userService.createUser(user)).isInstanceOf(
+            UserExistException.class);
 
     }
 
