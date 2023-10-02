c8a41d1f0ce8291c4136b588aa33e6734de31235
suchshin <scshin97@gmail.com>
2023-10-02 16:27:46 +0900

    :heavy_plus_sign: Feat(linking/MyPage&RankingPage): 마이페이지와 랭킹 페이지 연결

--- frontend/src/screens/Main/index.js
+++ frontend/src/screens/Main/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import { StartingGame } from "./template";
+import { StartingGame, MyPage, RankingPage } from "./template";
 
 const MainStack = createStackNavigator();
 
 export default function Main() {
   return (
     <MainStack.Navigator>
-      <MainStack.Screen name="StartingGame" component={StartingGame} />
+      <MainStack.Screen name="StartingGame" component={StartingGame}  />
+      <MainStack.Screen name="MyPage" component={MyPage} />
+      <MainStack.Screen name="RankingPage" component={RankingPage} />
     </MainStack.Navigator>
   );
 }
