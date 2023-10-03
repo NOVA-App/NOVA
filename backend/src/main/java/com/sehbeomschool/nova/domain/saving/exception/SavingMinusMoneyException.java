63caa6a76905daf77a6d3e4429ea007bc8403fc0
limzzum <chfhddl1234@naver.com>
2023-10-04 00:53:12 +0900

    :heavy_plus_sign: Feat(saving) : 금융 마이너스 머니 예외 추가

---
+++ backend/src/main/java/com/sehbeomschool/nova/domain/saving/exception/SavingMinusMoneyException.java
@@ -0,0 +1,8 @@
+package com.sehbeomschool.nova.domain.saving.exception;
+
+public class SavingMinusMoneyException extends IllegalArgumentException {
+
+    public SavingMinusMoneyException(String message) {
+        super(message);
+    }
+}
