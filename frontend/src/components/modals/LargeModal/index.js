6ba01566e0fbed981528154599dd6670ba6fa1e4
김의준 <euijun98@naver.com>
2023-10-03 12:15:08 +0900

    :heavy_plus_sign: Feat(main/API): 월고정액 수정 API 연결

--- frontend/src/components/modals/LargeModal/index.js
+++ frontend/src/components/modals/LargeModal/index.js
@@ -1,35 +1,29 @@
-import { Modal, Dimensions, Text, StyleSheet } from 'react-native';
-import styled from 'styled-components/native';
-import * as S from './style'
+import { Modal, Dimensions, Text, StyleSheet } from "react-native";
+import styled from "styled-components/native";
+import * as S from "./style";
 
-
-const LargeModal = props => {
+const LargeModal = (props) => {
   const styles = StyleSheet.create({
-    Text:{
+    Text: {
       fontSize: 40,
-      Colors: 'black',
-    }
-  })
+      Colors: "black",
+    },
+  });
   return (
     // <Modal animationType="fade"  // 스타일 둘 중 하나 택 1
-    <Modal animationType="slide" 
-      transparent={true} 
-      visible={props.isVisible}
-      >
-    <S.ModalContent>
-      <S.TitleContainer>
-        <Text 
-          style={styles.Text} 
-          onPress={props.onClose}
-          > X</Text>
-      </S.TitleContainer>
-      <Text style={{ fontSize: 14 }}>{props.title}</Text>
-    </S.ModalContent>
-  </Modal>
-  )
-}
-
-
+    <Modal animationType="slide" transparent={true} visible={props.isVisible}>
+      <S.ModalContent>
+        <S.TitleContainer>
+          <Text style={styles.Text} onPress={props.onClose}>
+            {" "}
+            X
+          </Text>
+        </S.TitleContainer>
+        <Text style={{ fontSize: 14 }}>{props.title}</Text>
+      </S.ModalContent>
+    </Modal>
+  );
+};
 
 //   const styledTitleText = styled.Text`
 //   color: '#fff';
