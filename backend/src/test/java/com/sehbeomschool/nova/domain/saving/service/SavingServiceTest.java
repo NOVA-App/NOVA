1422c16ff3058c7e97b326d1fd42ffbbf73353f5
limzzum <chfhddl1234@naver.com>
2023-10-03 18:18:12 +0900

    :white_check_mark: Test(saving) : 금융 현황 조회 게임이 있는 경우 테스트

--- backend/src/test/java/com/sehbeomschool/nova/domain/saving/service/SavingServiceTest.java
+++ backend/src/test/java/com/sehbeomschool/nova/domain/saving/service/SavingServiceTest.java
@@ -3,11 +3,16 @@ package com.sehbeomschool.nova.domain.saving.service;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
 import com.sehbeomschool.nova.domain.game.dao.GameRepository;
+import com.sehbeomschool.nova.domain.game.domain.Game;
+import com.sehbeomschool.nova.domain.game.domain.MyAssets;
 import com.sehbeomschool.nova.domain.game.exception.GameNotFoundException;
 import com.sehbeomschool.nova.domain.saving.dao.InsInterestRepository;
 import com.sehbeomschool.nova.domain.saving.dao.SavingRepository;
+import com.sehbeomschool.nova.domain.saving.domain.InstallmentSavings;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -52,10 +57,28 @@ class SavingServiceTest {
             }
         }
 
-        @Test
-        void readSavingInfo() {
+        @Nested
+        @DisplayName("게임이 존재하면")
+        class exist_game {
 
+            @Test
+            @DisplayName("금융현황을 반환한다.")
+            void readSavingInfo() {
+
+                InstallmentSavings installmentSavings = InstallmentSavings.builder().build();
+                Game game = Game.builder().myAssets(MyAssets.builder().build()).build();
+                given(gameRepository.findById(any())).willReturn(Optional.of(game));
+                given(savingRepository.findByGameId(any())).willReturn(
+                    Optional.of(List.of(installmentSavings)));
+
+                savingService.readSavingInfo(any());
+
+                then(gameRepository).should().findById(any());
+                then(savingRepository).should().findByGameId(any());
+
+            }
         }
+
     }
 
 
