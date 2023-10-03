0bef5bf0662eb54c8e279d21376572c574171da0
김의준 <euijun98@naver.com>
2023-10-03 21:29:59 +0900

    :heavy_plus_sign: Feat(stock/detail): stock API

--- frontend/src/screens/Game/template/StockPage/StockDetailPage/index.js
+++ frontend/src/screens/Game/template/StockPage/StockDetailPage/index.js
@@ -1,39 +1,27 @@
 import React, { useState, useEffect } from "react";
-import { Dimensions, View, Text, TextInput, TouchableOpacity } from "react-native";
-import * as S from './style';
-import axios from 'axios';
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import * as S from "./style";
+import axios from "axios";
 import API_URL from "../../../../../../config";
-import { gameDataState } from "../../../../../recoil/recoil"
-import { useRecoilState, useRecoilValue } from "recoil";
+import { gameIdState } from "../../../../../recoil/recoil";
+import { useRecoilState } from "recoil";
 
-const { height, width } = Dimensions.get("window");
+const StockDetailPage = (props) => {
+  const [buyAmount, setBuyAmount] = useState(0);
+  const [adjustAmount, setAdjustAmount] = useState(0);
+  const [stockInfo, setStockInfo] = useState({});
+  const [gameId] = useRecoilState(gameIdState);
 
-const StockDetailPage = ({ stockId }) => {
-  const [buyAmount, setBuyAmount] = useState("0");
-  const [adjustAmount, setAdjustAmount] = useState("0");
-  const [stockInfo, setStockInfo] = useState(null);
-  const gameData = useRecoilValue(gameDataState);
-  const gameId = gameData.gameId
   useEffect(() => {
-    // Replace the URL with your actual API endpoint
-        // Mock gameId and stockId, replace them with the actual values you want to use
-
-    // Fetch stock information
-    const fetchData = async () => {
-      console.log(gameData.gameId)
-      console.log(stockId)
-      try {
-        const response = await axios.get(`${API_URL}/api/stock/${gameData.gameId}/${stockId}`);
-        console.log(response.data)
-        setStockInfo(response.data.stockInfo);
-        console.log(stockInfo)
-      } catch (error) {
-        console.error('API 호출 오류:', error);
-      }
-    };
-
-    fetchData();
-  }, [gameData.gameId, stockId]); // Empty dependency array means this effect runs once when the component mounts
+    axios
+      .get(`${API_URL}/api/stock/${gameId}/${props.route.params.stockId}`)
+      .then((response) => {
+        setStockInfo(response.data.data);
+      })
+      .catch((error) => {
+        console.error("데이터를 가져오는 동안 오류 발생: ", error);
+      });
+  });
 
   const handleBuyAmountChange = (text) => {
     setBuyAmount(text);
@@ -69,32 +57,66 @@ const StockDetailPage = ({ stockId }) => {
       <S.Container style={{ flex: 8.5, minWidth: "80%" }}>
         <View style={{ flex: 8, alignItems: "center" }}>
           <View style={{ marginTop: "5%" }}></View>
-          <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 10 }}>{stockInfo.stockName}</Text>
+          <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 10 }}>
+            {stockInfo.stockName}
+          </Text>
           {/* Line Chart Component */}
           {/* Replace with your line chart component */}
-          <View style={{ width: "80%", height: 100, backgroundColor: "lightgray", marginBottom: 20 }}></View>
+          <View
+            style={{
+              width: "80%",
+              height: 100,
+              backgroundColor: "lightgray",
+              marginBottom: 20,
+            }}
+          ></View>
           <Text>{`현재가: ${stockInfo.evaluation}`}</Text>
           <Text>{`상승률: ${stockInfo.fluctuations}%`}</Text>
           <Text>{`내 보유량: ${stockInfo.myQuantity}`}</Text>
           <Text>{`여유자산: 150,000,000원`}</Text>
-          <View style={{ flexDirection: "row", alignItems: "center", marginTop: 10 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: 10,
+            }}
+          >
             <View style={{ flex: 1, maxWidth: 120 }}>
               <TextInput
-                style={{ height: 40, borderColor: "gray", borderWidth: 1, paddingHorizontal: 10, marginRight: 10 }}
+                style={{
+                  height: 40,
+                  borderColor: "gray",
+                  borderWidth: 1,
+                  paddingHorizontal: 10,
+                  marginRight: 10,
+                }}
                 onChangeText={handleAdjustAmountChange}
                 value={adjustAmount}
                 keyboardType="numeric"
                 placeholder="수량 조정"
               />
             </View>
-            <TouchableOpacity onPress={() => handleAdjustQuantity(parseInt(adjustAmount))}>
+            <TouchableOpacity
+              onPress={() => handleAdjustQuantity(parseInt(adjustAmount))}
+            >
               <Text style={{ fontSize: 20 }}>수량</Text>
             </TouchableOpacity>
           </View>
-          <View style={{ flexDirection: "row", alignItems: "center", marginTop: 20 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: 20,
+            }}
+          >
             <View style={{ flex: 1, maxWidth: 120 }}>
               <TextInput
-                style={{ height: 40, borderColor: "gray", borderWidth: 1, paddingHorizontal: 10 }}
+                style={{
+                  height: 40,
+                  borderColor: "gray",
+                  borderWidth: 1,
+                  paddingHorizontal: 10,
+                }}
                 onChangeText={handleBuyAmountChange}
                 value={buyAmount}
                 keyboardType="numeric"
@@ -110,11 +132,34 @@ const StockDetailPage = ({ stockId }) => {
               </TouchableOpacity>
             </View>
           </View>
-          <View style={{ marginTop: 10, marginBottom: 10, flexDirection: "row", justifyContent: "space-around" }}>
-            <TouchableOpacity onPress={handleBuy} style={{ backgroundColor: "#0046FF", padding: 10, borderRadius: 5, width: "40%" }}>
+          <View
+            style={{
+              marginTop: 10,
+              marginBottom: 10,
+              flexDirection: "row",
+              justifyContent: "space-around",
+            }}
+          >
+            <TouchableOpacity
+              onPress={handleBuy}
+              style={{
+                backgroundColor: "#0046FF",
+                padding: 10,
+                borderRadius: 5,
+                width: "40%",
+              }}
+            >
               <Text style={{ color: "white" }}>매수하기</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={handleSell} style={{ backgroundColor: "#D90452", padding: 10, borderRadius: 5, width: "40%" }}>
+            <TouchableOpacity
+              onPress={handleSell}
+              style={{
+                backgroundColor: "#D90452",
+                padding: 10,
+                borderRadius: 5,
+                width: "40%",
+              }}
+            >
               <Text style={{ color: "white" }}>매도하기</Text>
             </TouchableOpacity>
           </View>
