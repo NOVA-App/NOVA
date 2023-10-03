aadd5f01e7bde1bbf7aa3c576597491b68054933
limzzum <chfhddl1234@naver.com>
2023-10-03 17:57:41 +0900

    :white_check_mark: Test(saving) : 금융 현황 조회 게임이 없는 경우 테스트

---
+++ backend/src/test/java/com/sehbeomschool/nova/domain/saving/service/SavingServiceTest.java
@@ -0,0 +1,89 @@
+package com.sehbeomschool.nova.domain.saving.service;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import com.sehbeomschool.nova.domain.game.dao.GameRepository;
+import com.sehbeomschool.nova.domain.game.exception.GameNotFoundException;
+import com.sehbeomschool.nova.domain.saving.dao.InsInterestRepository;
+import com.sehbeomschool.nova.domain.saving.dao.SavingRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("금융 서비스 단위 테스트")
+class SavingServiceTest {
+
+    @InjectMocks
+    private SavingServiceImpl savingService;
+
+    @Mock
+    private SavingRepository savingRepository;
+
+    @Mock
+    private GameRepository gameRepository;
+
+    @Mock
+    private InsInterestRepository insInterestRepository;
+
+    @Nested
+    @DisplayName("나의 금융 현황 불러오기 with gameId")
+    class readSavingInfo {
+
+        @Nested
+        @DisplayName("게임이 존재하지 않으면")
+        class not_exist_game {
+
+            @Test
+            @DisplayName("게임 존재하지 않음 예외 발생")
+            void exception() {
+                given(gameRepository.findById(any())).willReturn(Optional.empty());
+
+                assertThatThrownBy(() -> savingService.readSavingInfo(any())).isInstanceOf(
+                    GameNotFoundException.class
+                );
+            }
+        }
+
+        @Test
+        void readSavingInfo() {
+
+        }
+    }
+
+
+    @Test
+    void createInstallment() {
+    }
+
+    @Test
+    void deleteInstallment() {
+    }
+
+    @Test
+    void matureInstallment() {
+    }
+
+    @Test
+    void updateInstallmentForNextYear() {
+    }
+
+    @Test
+    void updateIrpCost() {
+    }
+
+    @Test
+    void updateIrpForNextYear() {
+    }
+
+    @Test
+    void deleteInstallmentByGameId() {
+    }
+}
