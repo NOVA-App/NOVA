9c6513afcad0befb028532ea3d6e000ab4281a41
김연재 <yjkim9195@gmail.com>
2023-10-04 18:00:46 +0900

    :heavy_plus_sign: Feat(Modal/IRP): IRP 설명 모달

--- frontend/src/components/IRPCards/index.js
+++ frontend/src/components/IRPCards/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, ScrollView, Image, Dimensions } from "react-native";
+import { View, Text, ScrollView, Image, Dimensions, TouchableOpacity } from "react-native";
 import * as S from "./style";
 import SmallButton from "../buttons/SmallButton/index";
 import InputSmall from "../input/SmallInput";
@@ -7,12 +7,18 @@ import axios from "axios"; // axios 라이브러리 추가
 import API_URL from "../../../config";
 import { useRecoilValue } from "recoil";
 import { gameIdState } from "../../recoil/recoil";
+import SmallModal from "../modals/SmallModal";
 
 const { height, width } = Dimensions.get("window");
 
 const IRPCard = (props) => {
   const [irpCost, setIrpCost] = useState("");
   const gameID = useRecoilValue(gameIdState)
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
 
   const handleIrpPayment = () => {
     if (irpCost.trim() === "") {  //입력값이 비었을 떄
@@ -34,11 +40,34 @@ const IRPCard = (props) => {
 
   return (
     <S.Container height={height * 0.4}>
-      <S.TagContainer>
-        <Text style={{ fontSize: 20, color: "white" }}>IRP</Text>
-      </S.TagContainer>
-      <S.InfoContainer>
-        <Text style={{ fontSize: 22, color: "red" }}>?</Text>
+
+    <SmallModal
+      isVisible={isModalVisible}
+      onClose={onModalClose}
+      title={`      개인 퇴직 연금
+
+      근로자가 퇴직 또는 이직 시 받는 
+      퇴직금 일시금을 수령하는 계좌.
+
+      납입만 해도 세액 공제 혜택을 받을 수 있습니다. 
+
+      하지만 55세 이전 중도중도 해지할 경우, 
+      세제 혜택을 모두 반환해야 합니다.
+
+      55세 이후
+      연금 보험에 가입하여 수령할 수도 있고, 
+      기간과 비율을 지정하여 수령할 수도 있습니다.
+      `}
+    />
+        <S.TagContainer>
+          <Text style={{ fontSize: 20, color: "white" }}>IRP</Text>
+        </S.TagContainer>
+      <S.InfoContainer >
+      <TouchableOpacity onPress={() => {
+        setIsModalVisible(true);
+      }}>
+        <Text style={{ fontSize: 25, color: "red", fontWeight: 'bold' }}>?</Text>
+      </TouchableOpacity>
       </S.InfoContainer>
 
       <S.SmallContainer>
