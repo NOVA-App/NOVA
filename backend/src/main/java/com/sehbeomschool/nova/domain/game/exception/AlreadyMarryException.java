6e759fcc180359e85a5df013e260d2df66ed6cb7
limzzum <chfhddl1234@naver.com>
2023-10-05 20:55:22 +0900

    :heavy_plus_sign: Feat(game) : 이미 결혼한 상태 예외 추가

---
+++ backend/src/main/java/com/sehbeomschool/nova/domain/game/exception/AlreadyMarryException.java
@@ -0,0 +1,9 @@
+package com.sehbeomschool.nova.domain.game.exception;
+
+public class AlreadyMarryException extends IllegalArgumentException {
+
+    public AlreadyMarryException(String message) {
+        super(message);
+    }
+
+}
