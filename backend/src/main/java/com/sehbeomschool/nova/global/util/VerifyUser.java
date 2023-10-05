932d4204e86a60ff2f6dbba1f5d00ea7989477db
limzzum <chfhddl1234@naver.com>
2023-10-05 20:58:19 +0900

    :hammer: Refactor : 단어 수정

--- backend/src/main/java/com/sehbeomschool/nova/global/util/VerifyUser.java
+++ backend/src/main/java/com/sehbeomschool/nova/global/util/VerifyUser.java
@@ -1,16 +1,16 @@
 package com.sehbeomschool.nova.global.util;
 
 import com.sehbeomschool.nova.global.error.constant.ExceptionMessage;
-import com.sehbeomschool.nova.global.error.exception.NotVarifiedUserException;
+import com.sehbeomschool.nova.global.error.exception.NotVerifiedUserException;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-public class VarifyUser {
+public class VerifyUser {
 
-    public static void varifyUser(Long userId) {
+    public static void verifyUser(Long userId) {
         Long curUserId = (Long) SecurityContextHolder.getContext().getAuthentication()
             .getPrincipal();
         if (!curUserId.equals(userId)) {
-            throw new NotVarifiedUserException(ExceptionMessage.NOT_VARIFIED_USER.getMessage());
+            throw new NotVerifiedUserException(ExceptionMessage.NOT_VARIFIED_USER.getMessage());
         }
     }
 }
