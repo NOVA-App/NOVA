0621c3937d0d0399513ec8a34b28714a706b9a06
김연재 <yjkim9195@gmail.com>
2023-10-05 14:20:32 +0900

    :bug: Fix(page/stock): 주식 누락된 것 수정, % 이상 수정

--- frontend/src/components/stockpage/StockTradeComponent/StockCard/index.js
+++ frontend/src/components/stockpage/StockTradeComponent/StockCard/index.js
@@ -7,24 +7,26 @@ import { useNavigation } from "@react-navigation/native";
 const StockCard = (props) => {
   const navigation = useNavigation();
 
-  const rate = Math.floor(
-    (props.fluctuations * 100) / (props.stockAmount - props.fluctuations)
-  );
-
   const handleBuyButtonPress = () => {
     navigation.navigate("StockDetailPage", {
       screen: "StockDetailPage",
       stockId: props.stockItem.stockId,
-      rate: rate,
+      rate: props.fluctuations,
     });
   };
+
   return (
     <S.Container>
       <S.ContentContainer>
         <S.MiddleText>{props.stockName}</S.MiddleText>
-        <S.MiddleText>{[props.stockAmount].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</S.MiddleText>
+        <S.MiddleText>
+          {[props.stockAmount].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+        </S.MiddleText>
         <Text style={{ fontSize: 25, color: "#D90452" }}>
-          {rate < 0 ? rate : `+${rate}`}%
+          {props.fluctuations < 0
+            ? props.fluctuations
+            : `+${props.fluctuations}`}
+          %
         </Text>
       </S.ContentContainer>
       <View
